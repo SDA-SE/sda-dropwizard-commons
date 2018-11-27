7d70103950728612b32d17a5824588d9070ed7ef
Jörg Siebahn <joerg.siebahn@sda-se.com>
2018-11-27 11:29:42 +0100

    feat(client-jersey): Add utility to handle errors when using the generic client

---
+++ sda-commons-client-jersey/src/integTest/java/org/sdase/commons/client/jersey/ClientErrorUtilTest.java
@@ -0,0 +1,201 @@
+package org.sdase.commons.client.jersey;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
+import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.sdase.commons.client.jersey.error.ClientErrorUtil;
+import org.sdase.commons.client.jersey.error.ClientRequestException;
+
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Map;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonMap;
+import static javax.ws.rs.core.HttpHeaders.ACCEPT;
+import static javax.ws.rs.core.HttpHeaders.CONTENT_TYPE;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.entry;
+
+public class ClientErrorUtilTest {
+
+   @ClassRule
+   public static final WireMockClassRule WIRE = new WireMockClassRule(wireMockConfig().dynamicPort());
+
+   private static final ObjectMapper OM = new ObjectMapper();
+
+   private WebTarget webTarget;
+   @Before
+   public void resetRequests() {
+      WIRE.resetAll();
+      webTarget = JerseyClientBuilder.createClient().target(WIRE.baseUrl());
+   }
+
+   @Test
+   public void catchNotFoundException() {
+      WIRE.stubFor(
+            get("/").withHeader(ACCEPT, equalTo(APPLICATION_JSON))
+                  .willReturn(aResponse()
+                        .withStatus(404)
+                        .withHeader(CONTENT_TYPE, TEXT_PLAIN) // https://github.com/dropwizard/dropwizard/issues/231
+                        .withBody("Not Found")
+                  )
+      );
+
+      assertThatExceptionOfType(ClientRequestException.class).isThrownBy(
+            () -> ClientErrorUtil.convertExceptions(() -> webTarget.request(APPLICATION_JSON).get(String.class))
+      ).withCauseInstanceOf(NotFoundException.class);
+   }
+
+   @Test
+   public void passThroughTheResponseEntity() throws JsonProcessingException {
+      WIRE.stubFor(
+            get("/").withHeader(ACCEPT, equalTo(APPLICATION_JSON))
+                  .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON)
+                        .withBody(OM.writeValueAsBytes(singletonMap("message", "This has been found"))) // NOSONAR
+                  )
+      );
+
+      Map<String, String> actual = ClientErrorUtil.convertExceptions(
+            () -> webTarget.request(APPLICATION_JSON).get(new GenericType<Map<String, String>>() {})
+      );
+      assertThat(actual).containsExactly(entry("message", "This has been found"));
+   }
+
+   @Test
+   public void readErrorEntityWithJacksonReference() throws JsonProcessingException {
+      WIRE.stubFor(
+            get("/").withHeader(ACCEPT, equalTo(APPLICATION_JSON))
+                  .willReturn(aResponse()
+                        .withStatus(400)
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON)
+                        .withBody(OM.writeValueAsBytes(asList("Error number one", "Error number two"))) // NOSONAR
+                  )
+      );
+      
+      Object responseBody = null;
+      List<String> errors = null;
+      try {
+         responseBody = ClientErrorUtil.convertExceptions(() -> webTarget.request(APPLICATION_JSON).get(String.class));
+      }
+      catch (ClientRequestException e) {
+         errors = ClientErrorUtil.readErrorBody(e, new TypeReference<List<String>>() {});
+      }
+      assertThat(errors).isNotNull().containsExactly("Error number one", "Error number two");
+      //noinspection ConstantConditions
+      assertThat(responseBody).isNull();
+   }
+
+   @Test
+   public void readErrorEntityWithJaxRsReference() throws JsonProcessingException {
+      WIRE.stubFor(
+            get("/").withHeader(ACCEPT, equalTo(APPLICATION_JSON))
+                  .willReturn(aResponse()
+                        .withStatus(400)
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON)
+                        .withBody(OM.writeValueAsBytes(asList("Error number one", "Error number two")))
+                  )
+      );
+
+      Object responseBody = null;
+      List<String> errors = null;
+      try {
+         responseBody = ClientErrorUtil.convertExceptions(() -> webTarget.request(APPLICATION_JSON).get(String.class));
+      }
+      catch (ClientRequestException e) {
+         errors = ClientErrorUtil.readErrorBody(e, new GenericType<List<String>>() {});
+      }
+      assertThat(errors).isNotNull().containsExactly("Error number one", "Error number two");
+      //noinspection ConstantConditions
+      assertThat(responseBody).isNull();
+   }
+
+   @Test
+   public void readErrorEntityWithClassReference() {
+      WIRE.stubFor(
+            get("/").withHeader(ACCEPT, equalTo(APPLICATION_JSON))
+                  .willReturn(aResponse()
+                        .withStatus(400)
+                        .withHeader(CONTENT_TYPE, TEXT_PLAIN) // https://github.com/dropwizard/dropwizard/issues/231
+                        .withBody("Error number one\nError number two")
+                  )
+      );
+
+      Object responseBody = null;
+      String errors = null;
+      try {
+         responseBody = ClientErrorUtil.convertExceptions(() -> webTarget.request(APPLICATION_JSON).get(String.class));
+      }
+      catch (ClientRequestException e) {
+         errors = ClientErrorUtil.readErrorBody(e, String.class);
+      }
+      assertThat(errors).isEqualTo("Error number one\nError number two");
+      assertThat(responseBody).isNull();
+   }
+
+   @Test
+   public void readNoErrorEntityAsTypeReferenceFromSuccessfulResponse() throws JsonProcessingException {
+      WIRE.stubFor(
+            get("/").withHeader(ACCEPT, equalTo(APPLICATION_JSON))
+                  .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON)
+                        .withBody(OM.writeValueAsBytes(singletonMap("message", "This has been found")))
+                  )
+      );
+
+      Response response = webTarget.request(APPLICATION_JSON).get();
+      Map<String, Object> errorBody = ClientErrorUtil.readErrorBody(response, new TypeReference<Map<String, Object>>() {});
+      assertThat(errorBody).isNull();
+   }
+
+   @Test
+   public void readNoErrorEntityAsGenericTypeFromSuccessfulResponse() throws JsonProcessingException {
+      WIRE.stubFor(
+            get("/").withHeader(ACCEPT, equalTo(APPLICATION_JSON))
+                  .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON)
+                        .withBody(OM.writeValueAsBytes(singletonMap("message", "This has been found")))
+                  )
+      );
+
+      Response response = webTarget.request(APPLICATION_JSON).get();
+      Map<String, Object> errorBody = ClientErrorUtil.readErrorBody(response, new GenericType<Map<String, Object>>() {});
+      assertThat(errorBody).isNull();
+   }
+
+   @Test
+   public void readNoErrorEntityAsClassFromSuccessfulResponse() {
+      WIRE.stubFor(
+            get("/").withHeader(ACCEPT, equalTo(TEXT_PLAIN))
+                  .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader(CONTENT_TYPE, TEXT_PLAIN) // https://github.com/dropwizard/dropwizard/issues/231
+                        .withBody("This has been found")
+                  )
+      );
+
+      Response response = webTarget.request(TEXT_PLAIN).get();
+      String errorBody = ClientErrorUtil.readErrorBody(response, String.class);
+      assertThat(errorBody).isNull();
+   }
+
+}
---
+++ sda-commons-client-jersey/src/main/java/org/sdase/commons/client/jersey/error/ClientErrorUtil.java
@@ -0,0 +1,250 @@
+package org.sdase.commons.client.jersey.error;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.apache.commons.lang3.NotImplementedException;
+
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+/**
+ * A utility class to support handling of errors that are received from an outgoing request.
+ */
+public class ClientErrorUtil {
+
+   private ClientErrorUtil() {
+   }
+
+   /**
+    *
+    * @param response the response that contains an error
+    * @return the error data
+    * @throws NotImplementedException until the method is implemented
+    */
+   public static Object readErrorBody(Response response) {
+      // TODO return SDA platform error type => ApiError with InvalidParameter's
+      throw new NotImplementedException("The Problem Api from the REST Guide is not implemented yet.");
+   }
+
+   /**
+    *
+    * @param e the error occurred when requesting a resource.
+    * @return the error data
+    * @throws NotImplementedException until the method is implemented
+    */
+   public static Object readErrorBody(ClientRequestException e) {
+      // TODO return SDA platform error type => ApiError with InvalidParameter's
+      throw new NotImplementedException("The Problem Api from the REST Guide is not implemented yet.");
+   }
+
+   /**
+    * Read json body of the given {@code response} as {@code errorType}.
+    *
+    * @param response the response that produced an error.
+    * @param errorType the expected type of the data in the response body
+    * @param <E> the type the response body is converted to
+    * @return the response body converted to the desired {@code errorType} or {@code null} if the response is neither a
+    *         client error (4xx) nor a server error (5xx).
+    * @throws ProcessingException   if the content of the message cannot be
+    *                               mapped to an entity of the requested type. See {@link Response#readEntity(Class)}
+    * @throws IllegalStateException if the entity is not backed by an input stream,
+    *                               the response has been {@link Response#close() closed} already,
+    *                               or if the entity input stream has been fully consumed already and has
+    *                               not been buffered prior consuming. See {@link Response#readEntity(Class)}
+    */
+   public static <E> E readErrorBody(Response response, Class<E> errorType) {
+      if (response.getStatus() > 399) {
+         return response.readEntity(errorType);
+      } else {
+         return null;
+      }
+   }
+
+   /**
+    * Read json body of the given {@code response} as {@code errorType}.
+    *
+    * @param response the response that produced an error.
+    * @param errorType the expected type of the data in the response body
+    * @param <E> the type the response body is converted to
+    * @return the response body converted to the desired {@code errorType} or {@code null} if the response is neither a
+    *         client error (4xx) nor a server error (5xx).
+    * @throws ProcessingException   if the content of the message cannot be
+    *                               mapped to an entity of the requested type. See {@link Response#readEntity(GenericType)}
+    * @throws IllegalStateException if the entity is not backed by an input stream,
+    *                               the response has been {@link Response#close() closed} already,
+    *                               or if the entity input stream has been fully consumed already and has
+    *                               not been buffered prior consuming. See {@link Response#readEntity(GenericType)}
+    */
+   public static <E> E readErrorBody(Response response, TypeReference<E> errorType) {
+      if (response.getStatus() > 399) {
+         return response.readEntity(new GenericType<>(errorType.getType()));
+      } else {
+         return null;
+      }
+   }
+
+   /**
+    * Read json body of the given {@code response} as {@code errorType}.
+    *
+    * @param response the response that produced an error.
+    * @param errorType the expected type of the data in the response body
+    * @param <E> the type the response body is converted to
+    * @return the response body converted to the desired {@code errorType} or {@code null} if the response is neither a
+    *         client error (4xx) nor a server error (5xx).
+    * @throws ProcessingException   if the content of the message cannot be
+    *                               mapped to an entity of the requested type. See {@link Response#readEntity(GenericType)}
+    * @throws IllegalStateException if the entity is not backed by an input stream,
+    *                               the response has been {@link Response#close() closed} already,
+    *                               or if the entity input stream has been fully consumed already and has
+    *                               not been buffered prior consuming. See {@link Response#readEntity(GenericType)}
+    */
+   public static <E> E readErrorBody(Response response, GenericType<E> errorType) {
+      if (response.getStatus() > 399) {
+         return response.readEntity(errorType);
+      } else {
+         return null;
+      }
+   }
+
+   /**
+    * Read json body of the request encapsulated in the given {@link ClientRequestException} as {@code errorType}.
+    *
+    * @param e the error occurred when requesting a resource.
+    * @param errorType the expected type of the data in the response body
+    * @param <E> the type the response body is converted to
+    * @return the response body converted to the desired {@code errorType} or {@code null} if the given
+    *         {@link ClientRequestException} does not contain a {@link WebApplicationException} with a {@link Response}
+    *         that is a client error (4xx) or a server error (5xx).
+    * @throws ProcessingException   if the content of the message cannot be
+    *                               mapped to an entity of the requested type. See {@link Response#readEntity(Class)}
+    * @throws IllegalStateException if the entity is not backed by an input stream,
+    *                               the response has been {@link Response#close() closed} already,
+    *                               or if the entity input stream has been fully consumed already and has
+    *                               not been buffered prior consuming. See {@link Response#readEntity(Class)}
+    */
+   public static <E> E readErrorBody(ClientRequestException e, Class<E> errorType) {
+      return Optional.ofNullable(e.getCause())
+            .map(WebApplicationException::getResponse)
+            .map(r -> readErrorBody(r, errorType))
+            .orElse(null);
+   }
+
+   /**
+    * Read json body of the request encapsulated in the given {@link ClientRequestException} as {@code errorType}.
+    *
+    * @param e the error occurred when requesting a resource.
+    * @param errorType the expected type of the data in the response body
+    * @param <E> the type the response body is converted to
+    * @return the response body converted to the desired {@code errorType} or {@code null} if the given
+    *         {@link ClientRequestException} does not contain a {@link WebApplicationException} with a {@link Response}
+    *         that is a client error (4xx) or a server error (5xx).
+    * @throws ProcessingException   if the content of the message cannot be
+    *                               mapped to an entity of the requested type. See {@link Response#readEntity(GenericType)}
+    * @throws IllegalStateException if the entity is not backed by an input stream,
+    *                               the response has been {@link Response#close() closed} already,
+    *                               or if the entity input stream has been fully consumed already and has
+    *                               not been buffered prior consuming. See {@link Response#readEntity(GenericType)}
+    */
+   public static <E> E readErrorBody(ClientRequestException e, TypeReference<E> errorType) {
+      return Optional.ofNullable(e.getCause())
+            .map(WebApplicationException::getResponse)
+            .map(r -> readErrorBody(r, errorType))
+            .orElse(null);
+   }
+
+   /**
+    * Read json body of the request encapsulated in the given {@link ClientRequestException} as {@code errorType}.
+    *
+    * @param e the error occurred when requesting a resource.
+    * @param errorType the expected type of the data in the response body
+    * @param <E> the type the response body is converted to
+    * @return the response body converted to the desired {@code errorType} or {@code null} if the given
+    *         {@link ClientRequestException} does not contain a {@link WebApplicationException} with a {@link Response}
+    *         that is a client error (4xx) or a server error (5xx).
+    * @throws ProcessingException   if the content of the message cannot be
+    *                               mapped to an entity of the requested type. See {@link Response#readEntity(GenericType)}
+    * @throws IllegalStateException if the entity is not backed by an input stream,
+    *                               the response has been {@link Response#close() closed} already,
+    *                               or if the entity input stream has been fully consumed already and has
+    *                               not been buffered prior consuming. See {@link Response#readEntity(GenericType)}
+    */
+   public static <E> E readErrorBody(ClientRequestException e, GenericType<E> errorType) {
+      return Optional.ofNullable(e.getCause())
+            .map(WebApplicationException::getResponse)
+            .map(r -> readErrorBody(r, errorType))
+            .orElse(null);
+   }
+
+   /**
+    * <p>
+    *    Wrapped around a request from a
+    *    {@link org.sdase.commons.client.jersey.builder.PlatformClientBuilder#buildGenericClient(String) platform generic client}
+    *    of a
+    *    {@link org.sdase.commons.client.jersey.builder.ExternalClientBuilder#buildGenericClient(String) external generic client}
+    *    to catch any {@link WebApplicationException} that is thrown during the request and convert it into
+    *    {@link ClientRequestException} that is properly handled by {@link javax.ws.rs.ext.ExceptionMapper}s for the
+    *    incoming request.
+    * </p>
+    * <p>
+    *    This conversion is not required for
+    *    {@link org.sdase.commons.client.jersey.builder.PlatformClientBuilder#api(Class) platform api clients} or
+    *    {@link org.sdase.commons.client.jersey.builder.ExternalClientBuilder#api(Class) external api clients} from
+    *    interfaces. They do this conversion automatically.
+    * </p>
+    *
+    * @param request the request lambda that is executing the request, e.g.
+    * <pre>
+    * <code> WebTarget carsTarget = clientBuilder.buildGenericClient("cars").target(CARS_TARGET)<br/>
+    * Car car = ClientErrorUtil.convertExceptions(<br/>
+    *         () -> carsTarget.path("api").path("cars").path("123")<br/>
+    *                 .request(MediaType.APPLICATION_JSON)<br/>
+    *                 .get(Car.class)<br/>
+    *   );
+    * </code>
+    * </pre>
+    * @param <T> the expected return type of the request body
+    *
+    * @return the response body converted to the desired type
+    *
+    * @throws ClientRequestException if any {@link WebApplicationException} is thrown
+    */
+   public static <T> T convertExceptions(ResponseSupplier<T> request) {
+      try {
+         return request.get();
+      }
+      catch (WebApplicationException e) {
+         throw new ClientRequestException(e);
+      }
+   }
+
+   /**
+    * A supplier that executes a request.
+    *
+    * @param <T> the expected response type
+    */
+   @FunctionalInterface
+   public interface ResponseSupplier<T> extends Supplier<T> {
+
+      /**
+       * @return the value that is read from the response body, see
+       *       <ul>
+       *          <li>{@link javax.ws.rs.client.Invocation.Builder#get(Class)}</li>
+       *          <li>{@link javax.ws.rs.client.Invocation.Builder#get(GenericType)}</li>
+       *          <li>{@link javax.ws.rs.client.Invocation.Builder#post(Entity, Class)}</li>
+       *          <li>{@link javax.ws.rs.client.Invocation.Builder#post(Entity, GenericType)}</li>
+       *          <li>{@link javax.ws.rs.client.Invocation.Builder#delete(Class)}</li>
+       *          <li>{@link javax.ws.rs.client.Invocation.Builder#delete(GenericType)}</li>
+       *       </ul>
+       * @throws WebApplicationException if the request failed
+       */
+      @Override
+      T get();
+   }
+
+
+
+}
