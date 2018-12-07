4365656da4c43c89472ac8183eef13670eec48f1
Jörg Siebahn <joerg.siebahn@sda-se.com>
2018-12-07 14:20:50 +0100

    test(trace-token): Add missing integration test case

--- sda-commons-server-trace/src/integTest/java/org/sdase/commons/server/trace/TraceTokenBundleTest.java
+++ sda-commons-server-trace/src/integTest/java/org/sdase/commons/server/trace/TraceTokenBundleTest.java
@@ -1,5 +1,6 @@
 package org.sdase.commons.server.trace;
 
+import io.dropwizard.Configuration;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.junit.ClassRule;
@@ -14,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TraceTokenBundleTest {
 
    @ClassRule
-   public static DropwizardAppRule DW = new DropwizardAppRule(
+   public static DropwizardAppRule<Configuration> DW = new DropwizardAppRule<>(
          TraceTokenTestApp.class, ResourceHelpers.resourceFilePath("test-config.yaml"));
 
    @Test
@@ -24,6 +25,15 @@ public class TraceTokenBundleTest {
             .header("Trace-Token", "test-trace-token")
             .get(String.class);
       assertThat(token).isEqualTo("test-trace-token");
+   }
+
+   @Test
+   public void shouldRespondWithGivenTraceToken() {
+      Response response = DW.client().target("http://localhost:" + DW.getLocalPort()).path("/api/token")
+            .request(APPLICATION_JSON)
+            .header("Trace-Token", "test-trace-token")
+            .get();
+      assertThat(response.getHeaderString("Trace-Token")).isEqualTo("test-trace-token");
    }
 
 
