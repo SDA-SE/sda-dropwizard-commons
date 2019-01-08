82b220f54722d32fadcc34292bbcf891375aa1c6
Jörg Siebahn <joerg.siebahn@sda-se.com>
2019-01-08 16:02:51 +0100

    chore(server-starter): Optimize documentation in example app

--- sda-commons-server-starter-example/src/main/java/org/sdase/commons/server/starter/example/SdaPlatformExampleApplication.java
+++ sda-commons-server-starter-example/src/main/java/org/sdase/commons/server/starter/example/SdaPlatformExampleApplication.java
@@ -21,19 +21,33 @@ public class SdaPlatformExampleApplication extends Application<SdaPlatformConfig
    public void initialize(Bootstrap<SdaPlatformConfiguration> bootstrap) {
       // Add the starter bundle with minimal configuration includes
       //   - Support for environment variables in config files
+      //     (from sda-commons-server-dropwizard)
+      //   - Adding a trace token to log messages
+      //     (from sda-commons-server-trace)
       //   - CORS for Domains as defined in the configuration (may be none)
+      //     (from sda-commons-server-cors)
       //   - Tolerant Jackson configuration
+      //     (from sda-commons-server-jackson)
+      //   - Custom error mappers for SDA compliant error messages
+      //     (from sda-commons-server-jackson)
       //   - Support for Json responses with reduced properties by /myResource?fields=id,...
+      //     (from sda-commons-server-jackson)
       //   - HAL support
+      //     (from sda-commons-server-jackson)
       //   - Security checks at startup
+      //     (from sda-commons-server-security)
       //   - Prometheus metrics
+      //     (from sda-commons-server-prometheus)
       //   - OpenID Connect authentication (use @PermitAll to allow only access for identified users)
+      //     (from sda-commons-server-auth)
       bootstrap.addBundle(SdaPlatformBundle.builder()
             // Use all defaults provided by the Server Starter module
             .usingSdaPlatformConfiguration()
             // Require a consumer token from the client. Only swagger.json/yaml is always accessible.
+            // (from sda-commons-server-consumer)
             .withRequiredConsumerToken()
             // Set the name of the service as title of the Swagger documentation
+            // (from sda-commons-server-swagger)
             .withSwaggerInfoTitle("SDA Platform Example Application")
             // Additional Swagger documentation properties may be set here until
             // the packages that should be scanned for Swagger documentation annotations are defined
