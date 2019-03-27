23cc23be459e077d76d34e7a0750c74160fa3661
Oliver Sand <oliver.sand@sda-se.com>
2019-03-27 13:21:53 +0100

    fix(server-kafka): Decrease log level for kafka health check
    Previously the health checks printed the admin client config on every check on INFO level. Now the level is set to WARN during the health check.

--- sda-commons-server-kafka/src/main/java/org/sdase/commons/server/kafka/health/KafkaHealthCheck.java
+++ sda-commons-server-kafka/src/main/java/org/sdase/commons/server/kafka/health/KafkaHealthCheck.java
@@ -1,18 +1,21 @@
 package org.sdase.commons.server.kafka.health;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import com.codahale.metrics.health.HealthCheck;
 import com.codahale.metrics.health.annotation.Async;
 import org.apache.kafka.clients.admin.AdminClient;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.sdase.commons.server.kafka.KafkaConfiguration;
 import org.sdase.commons.server.kafka.KafkaProperties;
 
 import java.util.concurrent.TimeUnit;
+import org.slf4j.LoggerFactory;
 
 
 @Async(period = 30, scheduleType = Async.ScheduleType.FIXED_DELAY)
 public class KafkaHealthCheck extends HealthCheck {
-
 
    private final KafkaConfiguration config;
 
@@ -22,10 +25,19 @@ public class KafkaHealthCheck extends HealthCheck {
 
    @Override
    protected Result check() {
+      // Set logger for config to WARN, as it would otherwise print the full
+      // config on every health check to the log.
+      Logger logger = (Logger) LoggerFactory.getLogger(AdminClientConfig.class);
+      Level oldLevel = logger.getLevel();
+      logger.setLevel(Level.WARN);
+
       try (AdminClient adminClient = AdminClient.create(KafkaProperties.forAdminClient(config))){
          adminClient.listTopics().names().get(2, TimeUnit.SECONDS);
       } catch (Exception e) {
          return Result.unhealthy("Connection to broker failed within 2 seconds");
+      }
+      finally {
+         logger.setLevel(oldLevel);
       }
       return Result.healthy();
    }
