d84adc50fecbca1430bb54bec31f1c00c98569a8
Juergen Schmidt <jueschmi@de.ibm.com>
2019-05-17 11:56:17 +0200

    refactor(server-kafka): add additional constructors to the ProcessingErrorRetryException

--- sda-commons-server-kafka/src/main/java/org/sdase/commons/server/kafka/consumer/strategies/retryprocessingerror/ProcessingErrorRetryException.java
+++ sda-commons-server-kafka/src/main/java/org/sdase/commons/server/kafka/consumer/strategies/retryprocessingerror/ProcessingErrorRetryException.java
@@ -1,7 +1,21 @@
 package org.sdase.commons.server.kafka.consumer.strategies.retryprocessingerror;
 
 public class ProcessingErrorRetryException extends RuntimeException {
+
   public ProcessingErrorRetryException(String message) {
     super(message);
+  }
+
+  public ProcessingErrorRetryException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public ProcessingErrorRetryException(Throwable cause) {
+    super(cause);
+  }
+
+  public ProcessingErrorRetryException(String message, Throwable cause, boolean enableSuppression,
+      boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
   }
 }
