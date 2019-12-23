fbc7cf6a96de71492c8f89eba16cb50f8ebedd82
Christopher Cudennec <christopher.cudennec@sda-se.com>
2019-12-23 07:13:05 +0100

    fix(server-swagger): Exclude JAX-RS API 1 from the classpath
    Fixes compiler issues when classes from JAX-RS 2 are not found.

---
+++ sda-commons-server-swagger/src/test/java/org/sdase/commons/server/swagger/JavaxWsRsJava11Test.java
@@ -0,0 +1,17 @@
+package org.sdase.commons.server.swagger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.ws.rs.core.Response.Status;
+import org.junit.Test;
+
+public class JavaxWsRsJava11Test {
+
+  /**
+   * This test makes sure RS API 2 is in the classpath.
+   */
+  @Test
+  public void shouldBeAbleToImportClassFromRsApi2() {
+    assertThat(Status.REQUEST_TIMEOUT).isNotNull();
+  }
+}
