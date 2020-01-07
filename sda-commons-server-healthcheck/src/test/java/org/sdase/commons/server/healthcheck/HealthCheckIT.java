19771d4000864b0c7465b697b5ba4b0d912bb2d2
Christopher Cudennec <christopher.cudennec@sda-se.com>
2020-01-07 13:26:55 +0100

    test(server-healthcheck): Reset state before each test

--- sda-commons-server-healthcheck/src/test/java/org/sdase/commons/server/healthcheck/HealthCheckIT.java
+++ sda-commons-server-healthcheck/src/test/java/org/sdase/commons/server/healthcheck/HealthCheckIT.java
@@ -3,6 +3,7 @@ package org.sdase.commons.server.healthcheck;
 import com.codahale.metrics.health.HealthCheck;
 import io.dropwizard.Configuration;
 import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.sdase.commons.server.healthcheck.helper.ExternalServiceHealthCheck;
@@ -10,7 +11,6 @@ import org.sdase.commons.server.testing.DropwizardRuleHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Response;
 
 import static javax.servlet.http.HttpServletResponse.SC_OK;
@@ -27,9 +27,13 @@ public class HealthCheckIT {
          .withRandomPorts()
          .build();
 
+   @Before
+   public void setUp() {
+      setApplicationHealth(true);
+   }
+
    @Test
    public void testAllHealthChecks() {
-      setApplicationHealth(true);
       Response response = healthCheckGet("/healthcheck");
       String metrics = response.readEntity(String.class);
       assertThat(metrics).contains(HealthApplication.DUMMY_EXTERNAL);
@@ -38,7 +42,6 @@ public class HealthCheckIT {
 
    @Test
    public void testHealthChecksInternal() {
-      setApplicationHealth(true);
       Response response = healthCheckGet("/healthcheck/internal");
       assertThat(response.getStatus()).isEqualTo(SC_OK);
       String metrics = response.readEntity(String.class);
