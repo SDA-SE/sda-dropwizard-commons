517f4be8ea8f8d6a5e79357747e7c141541f62d9
Jörg Siebahn <joerg.siebahn@sda-se.com>
2020-06-23 19:48:14 +0200

    test(auth): cover missing branches in OpaJwtPrincipalFactory

---
+++ sda-commons-server-auth/src/test/java/org/sdase/commons/server/opa/internal/OpaJwtPrincipalFactoryTest.java
@@ -0,0 +1,84 @@
+package org.sdase.commons.server.opa.internal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.auth0.jwt.interfaces.Claim;
+import java.security.Principal;
+import java.util.Map;
+import javax.ws.rs.core.SecurityContext;
+import org.junit.Test;
+import org.sdase.commons.server.opa.OpaJwtPrincipal;
+
+public class OpaJwtPrincipalFactoryTest {
+
+  private SecurityContext securityContextMock = mock(SecurityContext.class);
+  private OpaJwtPrincipalFactory opaJwtPrincipalFactory =
+      new OpaJwtPrincipalFactory(securityContextMock);
+
+  @Test
+  public void shouldProvideOpaJwtPrincipalFromSecurityContext() {
+
+    Principal given = emptyOpaJwtPrincipal();
+    when(securityContextMock.getUserPrincipal()).thenReturn(given);
+
+    OpaJwtPrincipal actual = opaJwtPrincipalFactory.provide();
+
+    assertThat(actual).isSameAs(given);
+  }
+
+  @Test
+  public void shouldSkipOtherTypeOfPrincipal() {
+
+    Principal given = emptyGenericPrincipal();
+    when(securityContextMock.getUserPrincipal()).thenReturn(given);
+
+    OpaJwtPrincipal actual = opaJwtPrincipalFactory.provide();
+
+    assertThat(actual).isNull();
+  }
+
+  @Test
+  public void shouldReturnNullIfNoPrincipal() {
+
+    when(securityContextMock.getUserPrincipal()).thenReturn(null);
+
+    OpaJwtPrincipal actual = opaJwtPrincipalFactory.provide();
+
+    assertThat(actual).isNull();
+  }
+
+  private Principal emptyGenericPrincipal() {
+    return () -> null;
+  }
+
+  private OpaJwtPrincipal emptyOpaJwtPrincipal() {
+    return new OpaJwtPrincipal() {
+      @Override
+      public String getJwt() {
+        return null;
+      }
+
+      @Override
+      public Map<String, Claim> getClaims() {
+        return null;
+      }
+
+      @Override
+      public String getConstraints() {
+        return null;
+      }
+
+      @Override
+      public <T> T getConstraintsAsEntity(Class<T> resultType) {
+        return null;
+      }
+
+      @Override
+      public String getName() {
+        return null;
+      }
+    };
+  }
+}
