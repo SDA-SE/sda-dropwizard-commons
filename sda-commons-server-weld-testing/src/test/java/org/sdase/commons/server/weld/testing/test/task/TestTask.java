9ae507c332a3e0b745c9a86dc4cd6344cb604ad4
Dominik Henneke <dominik.henneke@sda-se.com>
2020-07-07 18:51:10 +0200

    Update changed tasks interface (Multimap to map) and return value of cli.run and get rid of hamcrest

--- sda-commons-server-weld-testing/src/test/java/org/sdase/commons/server/weld/testing/test/task/TestTask.java
+++ sda-commons-server-weld-testing/src/test/java/org/sdase/commons/server/weld/testing/test/task/TestTask.java
@@ -1,8 +1,9 @@
 package org.sdase.commons.server.weld.testing.test.task;
 
-import com.google.common.collect.ImmutableMultimap;
 import io.dropwizard.servlets.tasks.Task;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
 import javax.inject.Inject;
 import org.sdase.commons.server.weld.testing.test.util.BarSupplier;
 
@@ -17,8 +18,7 @@ public class TestTask extends Task {
   }
 
   @Override
-  public void execute(ImmutableMultimap<String, String> parameters, PrintWriter output)
-      throws Exception {
+  public void execute(Map<String, List<String>> parameters, PrintWriter output) throws Exception {
     result = supplier;
   }
 
