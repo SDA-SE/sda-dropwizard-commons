e528165bb782609a995a8eaf189527eddea7c1e5
Christopher Cudennec <christopher.cudennec@sda-se.com>
2020-07-08 16:51:01 +0200

    fix(server-auth): OPA's policy exists health check handles null responses in case of misconfiguration

--- sda-commons-server-auth-testing/src/test/java/org/sdase/commons/server/opa/testing/PolicyExistsHealthCheckIT.java
+++ sda-commons-server-auth-testing/src/test/java/org/sdase/commons/server/opa/testing/PolicyExistsHealthCheckIT.java
@@ -1,8 +1,12 @@
 package org.sdase.commons.server.opa.testing;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.anyUrl;
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.sdase.commons.server.opa.testing.OpaRule.onAnyRequest;
 
+import com.codahale.metrics.health.HealthCheck.Result;
 import javax.ws.rs.client.WebTarget;
 import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.junit.Before;
@@ -14,7 +18,7 @@ import org.sdase.commons.server.opa.health.PolicyExistsHealthCheck;
 import org.sdase.commons.server.testing.Retry;
 import org.sdase.commons.server.testing.RetryRule;
 
-public class HealthCheckTest {
+public class PolicyExistsHealthCheckIT {
 
   @ClassRule public static final OpaRule OPA_RULE = new OpaRule();
   @Rule public RetryRule rule = new RetryRule();
@@ -42,6 +46,16 @@ public class HealthCheckTest {
   public void shouldBeUnhealthyIfOpaGivesEmptyResponse() {
     OPA_RULE.mock(onAnyRequest().emptyResponse());
     assertThat(policyExistsHealthCheck.check().isHealthy()).isFalse();
+  }
+
+  @Test
+  @Retry(5)
+  public void shouldBeUnhealthyIfOpaGivesNullResponse() {
+    OPA_RULE.mock(wire -> wire.stubFor(post(anyUrl()).willReturn(okJson("{}"))));
+    final Result check = policyExistsHealthCheck.check();
+    assertThat(check.isHealthy()).isFalse();
+    assertThat(check.getMessage())
+        .isEqualTo("The policy response seems not to be SDA guideline compliant");
   }
 
   @Test
