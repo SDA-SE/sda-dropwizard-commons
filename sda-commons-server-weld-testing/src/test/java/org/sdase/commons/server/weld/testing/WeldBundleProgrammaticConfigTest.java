bde5c78d37d5d32d6dbc82a3ca00dcd375958743
Dominik Henneke <dominik.henneke@sda-se.com>
2020-07-09 15:29:46 +0200

    feat: upgrade to Dropwizard 2.0.10
    This commit includes the dependency update to Dropwizard 2.0.x and resolves the following changes:
    * Register the CaffeineModule to the ObjectMapperConfigurationUtil since it was also added by Dropwizard
    * Incorporate the refactorings in newer versions of jersey
    * Switch the DataTypes and change from kilobytes to kibibytes in the BufferLimitsAdvice (See https://github.com/dropwizard/dropwizard/commit/184dadf82319ab4c6dc3237ddc303114e89c086c)
    * Fix some minor changes of AssertJ
    * Cleanup assertions since Bundle has been deprecated and now inherits from ConfiguredBundle<Configuration>
    * Fix changed interfaces for the Task and the CLI interface in tests of the weld module
    * Don't use two morphia bundles in a single application
    
    Co-authored-by: Oliver Sand <oliver.sand@sda-se.com>

--- sda-commons-server-weld-testing/src/test/java/org/sdase/commons/server/weld/testing/WeldBundleProgrammaticConfigTest.java
+++ sda-commons-server-weld-testing/src/test/java/org/sdase/commons/server/weld/testing/WeldBundleProgrammaticConfigTest.java
@@ -61,7 +61,7 @@ public class WeldBundleProgrammaticConfigTest {
 
     Cli cli = new Cli(location, bootstrap, stdOut, stdErr);
 
-    assertThat(cli.run("testDW"), equalTo(Boolean.TRUE));
+    assertThat(cli.run("testDW").isPresent(), equalTo(Boolean.FALSE));
 
     assertThat(app.getTestCommand().getResult(), equalTo("foo"));
   }
