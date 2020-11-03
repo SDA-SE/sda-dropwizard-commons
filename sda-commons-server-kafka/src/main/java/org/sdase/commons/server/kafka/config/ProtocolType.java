56b1101554460d55498806862556f20d7129a903
Dominik Henneke <dominik.henneke@sda-se.com>
2020-11-03 12:06:00 +0100

    fix(server-kafka): only configure the sasl settings if a sasl protocol is configured
    The service emits a warning in the log if the sasl.jaas.config property is set even if the security protocol doesn't support SASL:
    
    > The configuration 'sasl.jaas.config' was supplied but isn't a known config.

--- sda-commons-server-kafka/src/main/java/org/sdase/commons/server/kafka/config/ProtocolType.java
+++ sda-commons-server-kafka/src/main/java/org/sdase/commons/server/kafka/config/ProtocolType.java
@@ -4,5 +4,15 @@ public enum ProtocolType {
   PLAINTEXT,
   SSL,
   SASL_PLAINTEXT,
-  SASL_SSL
+  SASL_SSL;
+
+  /**
+   * Tells whether the {@link ProtocolType} is a SASL type or not.
+   *
+   * @param type the type to check
+   * @return if true, the type is a SASL type
+   */
+  public static boolean isSasl(ProtocolType type) {
+    return type == SASL_PLAINTEXT || type == SASL_SSL;
+  }
 }
