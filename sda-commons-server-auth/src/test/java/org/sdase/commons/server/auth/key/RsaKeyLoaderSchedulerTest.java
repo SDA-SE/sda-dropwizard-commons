aeaf1f80005ef7e942e3787d6516113eaea17aa0
Uwe Fuchs <info@uwefuchs.com>
2021-01-15 10:39:34 +0100

    test(server-auth): refactor scheduler tests

--- sda-commons-server-auth/src/test/java/org/sdase/commons/server/auth/key/RsaKeyLoaderSchedulerTest.java
+++ sda-commons-server-auth/src/test/java/org/sdase/commons/server/auth/key/RsaKeyLoaderSchedulerTest.java
@@ -1,12 +1,11 @@
 package org.sdase.commons.server.auth.key;
 
-import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 import static org.mockito.Mockito.mock;
 
 import java.security.interfaces.RSAPublicKey;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -14,94 +13,116 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RsaKeyLoaderSchedulerTest {
-  private static final int RELOAD_PERIOD = 2;
+  private static final int RELOAD_PERIOD = 1;
+
+  // test data
+  private final List<LoadedPublicKey> actualKeys = new ArrayList<>();
+  private final AtomicBoolean actualInvocationFails = new AtomicBoolean();
+
+  // track invocations
+  private final AtomicInteger numberOfReloads = new AtomicInteger(0);
+
+  // setup
   private final RsaPublicKeyLoader keyLoader = new RsaPublicKeyLoader();
   private final ScheduledExecutorService executorService =
       Executors.newSingleThreadScheduledExecutor();
   private final RsaKeyLoaderScheduler keyLoaderScheduler =
       RsaKeyLoaderScheduler.create(keyLoader, executorService);
+  private final KeySource keySource =
+      () -> {
+        numberOfReloads.incrementAndGet();
+        if (actualInvocationFails.get()) {
+          throw new RuntimeException("Some error occurred during reload");
+        }
+        return actualKeys;
+      };
 
-  @Test
-  public void shouldNotBeAuthenticatedWithRevokedKey() {
-    // given
-    final AtomicInteger numberOfCalls = new AtomicInteger(0);
-    final AtomicBoolean keyIsRevoked = new AtomicBoolean(false);
-    final RSAPublicKey validKey = mock(RSAPublicKey.class);
-    final String kidForValidKey = "kidForValidKey";
-    final RSAPublicKey keyToBeRevoked = mock(RSAPublicKey.class);
-    final String kidForKeyToBeRevoked = "kidForKeyToBeRevoked";
-
-    KeySource keySource =
-        new KeySource() {
-          @Override
-          public List<LoadedPublicKey> loadKeysFromSource() throws KeyLoadFailedException {
-            numberOfCalls.incrementAndGet();
-            if (!keyIsRevoked.get()) {
-              return Arrays.asList(
-                  new LoadedPublicKey(kidForValidKey, validKey, this),
-                  new LoadedPublicKey(kidForKeyToBeRevoked, keyToBeRevoked, this));
-            } else {
-              return singletonList(new LoadedPublicKey("kidForValidKey", validKey, this));
-            }
-          }
-        };
-
+  @Before
+  public void startKeyLoader() {
     keyLoader.addKeySource(keySource);
     keyLoaderScheduler.internalStart(0, RELOAD_PERIOD, TimeUnit.SECONDS);
-
-    await().untilAsserted(() -> assertThat(numberOfCalls.get()).isEqualTo(2));
-    assertThat(keyLoader.getKey(kidForValidKey)).isSameAs(validKey);
-    assertThat(keyLoader.getKey(kidForKeyToBeRevoked)).isSameAs(keyToBeRevoked);
-
-    // simulate revoking of key:
-    keyIsRevoked.set(true);
-    await().untilAsserted(() -> assertThat(numberOfCalls.get()).isEqualTo(3));
-    assertThat(keyLoader.getKey(kidForValidKey)).isSameAs(validKey);
-    // revoked key not found anymore
-    assertThat(keyLoader.getKey(kidForKeyToBeRevoked)).isNull();
-  }
-
-  @Test
-  public void shouldContinueJobProperlyAfterKeyLoadingFailure() {
-    final AtomicInteger numberOfCalls = new AtomicInteger(0);
-    final AtomicBoolean keyLoadShouldFail = new AtomicBoolean(false);
-    final AtomicBoolean anErrorHasOccurred = new AtomicBoolean(false);
-    final RSAPublicKey validKey = mock(RSAPublicKey.class);
-    final String kidForValidKey = "kidForValidKey";
-
-    KeySource keySource =
-        new KeySource() {
-          @Override
-          public List<LoadedPublicKey> loadKeysFromSource() throws KeyLoadFailedException {
-            numberOfCalls.incrementAndGet();
-            if (keyLoadShouldFail.get()) {
-              anErrorHasOccurred.set(true);
-              throw new KeyLoadFailedException("Some error occurred during reload");
-            } else {
-              return singletonList(new LoadedPublicKey(kidForValidKey, validKey, this));
-            }
-          }
-        };
-
-    keyLoader.addKeySource(keySource);
-    keyLoaderScheduler.internalStart(0, RELOAD_PERIOD, TimeUnit.SECONDS);
-
-    await().untilAsserted(() -> assertThat(numberOfCalls.get()).isEqualTo(2));
-    assertThat(keyLoader.getKey(kidForValidKey)).isSameAs(validKey);
-
-    // simulate keyLoad-failure:
-    keyLoadShouldFail.set(true);
-    await().untilAsserted(() -> assertThat(numberOfCalls.get()).isEqualTo(3));
-    assertThat(anErrorHasOccurred.get()).isEqualTo(true);
-    // key is found anyway:
-    assertThat(keyLoader.getKey(kidForValidKey)).isSameAs(validKey);
+    // wait for initial load
+    waitForNextReload();
   }
 
   @After
-  public void shutDown() {
+  public void shutdownExecutorService() {
     executorService.shutdown();
+  }
+
+  @Test
+  public void shouldRemoveRevokedKey() {
+    // given
+    RSAPublicKey validKey = mock(RSAPublicKey.class);
+    RSAPublicKey keyToBeRevoked = mock(RSAPublicKey.class);
+
+    // initially, two known keys are published
+    actualKeys.add(new LoadedPublicKey("kidForValidKey", validKey, keySource));
+    actualKeys.add(new LoadedPublicKey("kidForKeyToBeRevoked", keyToBeRevoked, keySource));
+
+    waitForNextReload();
+    assertThatKeyIsKnown("kidForValidKey", validKey);
+    assertThatKeyIsKnown("kidForKeyToBeRevoked", keyToBeRevoked);
+
+    // simulate revoking of key:
+    actualKeys.removeIf(loadedPublicKey -> loadedPublicKey.getKid().equals("kidForKeyToBeRevoked"));
+
+    waitForNextReload();
+    assertThatKeyIsKnown("kidForValidKey", validKey);
+    assertThatKeyIsUnknown("kidForKeyToBeRevoked");
+  }
+
+  @Test
+  public void shouldKeepKeysOnLoadingFailure() {
+    final RSAPublicKey validKey = mock(RSAPublicKey.class);
+
+    actualKeys.add(new LoadedPublicKey("kidForValidKey", validKey, keySource));
+
+    waitForNextReload();
+    assertThatKeyIsKnown("kidForValidKey", validKey);
+
+    // simulate keyLoad-failure:
+    actualInvocationFails.set(true);
+    waitForNextReload();
+    // key is found anyway:
+    assertThatKeyIsKnown("kidForValidKey", validKey);
+  }
+
+  @Test
+  public void shouldContinueScheduleAfterLoadFailure() {
+    final RSAPublicKey validKey = mock(RSAPublicKey.class);
+
+    // start empty
+    waitForNextReload();
+
+    // introduce error
+    actualInvocationFails.set(true);
+    waitForNextReload();
+
+    // expect more schedules
+    actualInvocationFails.set(false);
+    actualKeys.add(new LoadedPublicKey("kidForValidKey", validKey, keySource));
+
+    waitForNextReload();
+    assertThatKeyIsKnown("kidForValidKey", validKey);
+  }
+
+  private void assertThatKeyIsUnknown(String kid) {
+    assertThat(keyLoader.getKey(kid)).isNull();
+  }
+
+  private void assertThatKeyIsKnown(String kid, RSAPublicKey expectedKeyInstance) {
+    assertThat(keyLoader.getKey(kid)).isSameAs(expectedKeyInstance);
+  }
+
+  private void waitForNextReload() {
+    int expectedReloads = numberOfReloads.get() + 1;
+    await()
+        .untilAsserted(
+            () -> assertThat(numberOfReloads).hasValueGreaterThanOrEqualTo(expectedReloads));
   }
 }
