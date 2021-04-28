571826c6d345a0e6ed6422442f424624d841d5f6
Jörg Siebahn <joerg.siebahn@sda-se.com>
2021-04-28 16:47:03 +0200

    feat(auth): add ability to read static public keys
    In addition to read the public keys from static certificates, we are now able to read public keys as well.

--- sda-commons-server-auth/src/main/java/org/sdase/commons/server/auth/key/PemKeySource.java
+++ sda-commons-server-auth/src/main/java/org/sdase/commons/server/auth/key/PemKeySource.java
@@ -1,17 +1,24 @@
 package org.sdase.commons.server.auth.key;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,11 +39,17 @@ public class PemKeySource implements KeySource {
   public List<LoadedPublicKey> loadKeysFromSource() {
     try {
       LOG.info("Loading public key for token signature verification from PEM {}", pemKeyLocation);
-      X509Certificate cer = loadCertificate(pemKeyLocation);
-      RSAPublicKey publicKey = extractRsaPublicKey(cer);
-      LOG.info("Loaded public key for token signature verification from PEM {}", pemKeyLocation);
-      return Collections.singletonList(new LoadedPublicKey(kid, publicKey, this));
+      if (isPublicKey(pemKeyLocation)) {
+        RSAPublicKey publicKey = loadPublicKey(pemKeyLocation);
+        return Collections.singletonList(new LoadedPublicKey(kid, publicKey, this));
+      } else {
+        X509Certificate cer = loadCertificate(pemKeyLocation);
+        RSAPublicKey publicKey = extractRsaPublicKeyFromCertificate(cer);
+        LOG.info("Loaded public key for token signature verification from PEM {}", pemKeyLocation);
+        return Collections.singletonList(new LoadedPublicKey(kid, publicKey, this));
+      }
     } catch (IOException | CertificateException | NullPointerException | ClassCastException e) {
+
       throw new KeyLoadFailedException(
           "Failed to load public key for token signature verification from PEM " + pemKeyLocation,
           e);
@@ -76,7 +89,7 @@ public class PemKeySource implements KeySource {
     }
   }
 
-  private RSAPublicKey extractRsaPublicKey(X509Certificate certificate)
+  private RSAPublicKey extractRsaPublicKeyFromCertificate(X509Certificate certificate)
       throws KeyLoadFailedException { // NOSONAR
     RSAPublicKey publicKey;
     PublicKey cerPublicKey = certificate.getPublicKey();
@@ -90,5 +103,47 @@ public class PemKeySource implements KeySource {
               + pemKeyLocation);
     }
     return publicKey;
+  }
+
+  private boolean isPublicKey(URI pemKeyLocation) {
+    try (InputStream is = pemKeyLocation.toURL().openStream()) {
+      String keyContent = readContent(is).trim();
+      return keyContent.startsWith("-----BEGIN PUBLIC KEY-----")
+          && keyContent.endsWith("-----END PUBLIC KEY-----");
+    } catch (IOException e) {
+      throw new KeyLoadFailedException("Failed to read key from " + pemKeyLocation, e);
+    }
+  }
+
+  private RSAPublicKey loadPublicKey(URI pemKeyLocation) {
+    LOG.info("Loading public key for token signature verification from PEM {}", pemKeyLocation);
+    try (InputStream is = pemKeyLocation.toURL().openStream()) {
+      String pemPublicKeyContent = readContent(is);
+      String publicKeyPem =
+          pemPublicKeyContent
+              .replace("-----BEGIN PUBLIC KEY-----", "")
+              .replaceAll("(\\r\\n|\\r|\\n)", "")
+              .replace("-----END PUBLIC KEY-----", "");
+
+      byte[] encoded = Base64.decodeBase64(publicKeyPem);
+      X509EncodedKeySpec keySpec = new X509EncodedKeySpec(encoded);
+      final KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+      return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+    } catch (ClassCastException
+        | IOException
+        | NoSuchAlgorithmException
+        | InvalidKeySpecException e) {
+      throw new KeyLoadFailedException("Failed to load public key at " + pemKeyLocation, e);
+    }
+  }
+
+  private String readContent(InputStream content) throws IOException {
+    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+    int bytesRead;
+    byte[] data = new byte[1024];
+    while ((bytesRead = content.read(data, 0, data.length)) != -1) {
+      buffer.write(data, 0, bytesRead);
+    }
+    return new String(buffer.toByteArray(), StandardCharsets.UTF_8);
   }
 }
