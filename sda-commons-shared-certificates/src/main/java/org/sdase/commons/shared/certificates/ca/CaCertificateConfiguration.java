41f1c043a0875d3d725b20f74237bb73cda2336d
Ankita Srivastava <ankita.srivastava@sda.se>
2021-07-21 10:36:41 +0200

    docs(shared-certificates): add README documentation

--- sda-commons-shared-certificates/src/main/java/org/sdase/commons/shared/certificates/ca/CaCertificateConfiguration.java
+++ sda-commons-shared-certificates/src/main/java/org/sdase/commons/shared/certificates/ca/CaCertificateConfiguration.java
@@ -2,7 +2,7 @@ package org.sdase.commons.shared.certificates.ca;
 
 public class CaCertificateConfiguration {
   /**
-   * The directory that a CA certificate (list) in PEM format. This certificates are added to the
+   * The directory that contains CA certificates in PEM format. This certificates are added to the
    * {@link javax.net.ssl.TrustManager}s to verify the connection.
    */
   private String customCaCertificateDir;
