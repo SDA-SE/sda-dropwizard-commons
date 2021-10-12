ec78c6a14455669758dd106ad8d12e851331fa18
Christopher Cudennec <christopher.cudennec@sda-se.com>
2021-10-12 08:25:40 +0200

    test: Don't create topic (will be created automatically)

--- sda-commons-server-kafka/src/test/java/org/sdase/commons/server/kafka/KafkaBundleWithSaslScramIT.java
+++ sda-commons-server-kafka/src/test/java/org/sdase/commons/server/kafka/KafkaBundleWithSaslScramIT.java
@@ -63,8 +63,6 @@ public class KafkaBundleWithSaslScramIT {
 
   @BeforeClass
   public static void beforeClass() {
-    KAFKA.getKafkaTestUtils().createTopic("my-topic", 1, (short) 1);
-
     final KafkaProducer<String, String> producer =
         KAFKA.getKafkaTestUtils().getKafkaProducer(StringSerializer.class, StringSerializer.class);
     producer.send(new ProducerRecord<>("my-topic", "My Message"));
