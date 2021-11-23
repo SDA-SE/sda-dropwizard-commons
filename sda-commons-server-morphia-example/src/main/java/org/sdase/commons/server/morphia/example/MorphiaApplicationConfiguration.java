a8df4d6fcd83a8e54298b26ce59150938d58664e
Christopher Cudennec <christopher.cudennec@sda-se.com>
2021-11-23 16:37:45 +0100

    chore(server-morphia-example): Add ConfigurationSubstitutionBundle to example application

--- sda-commons-server-morphia-example/src/main/java/org/sdase/commons/server/morphia/example/MorphiaApplicationConfiguration.java
+++ sda-commons-server-morphia-example/src/main/java/org/sdase/commons/server/morphia/example/MorphiaApplicationConfiguration.java
@@ -1,6 +1,8 @@
 package org.sdase.commons.server.morphia.example;
 
 import io.dropwizard.Configuration;
+import javax.validation.Valid;
+import org.sdase.commons.server.auth.config.AuthConfig;
 import org.sdase.commons.server.morphia.MongoConfiguration;
 import org.sdase.commons.shared.certificates.ca.CaCertificateConfiguration;
 
@@ -8,7 +10,9 @@ import org.sdase.commons.shared.certificates.ca.CaCertificateConfiguration;
 public class MorphiaApplicationConfiguration extends Configuration {
 
   /** Configuration object from the morphia bundle: @{@link MongoConfiguration} */
-  private MongoConfiguration mongo = new MongoConfiguration();
+  @Valid private MongoConfiguration mongo = new MongoConfiguration();
+
+  @Valid private AuthConfig auth = new AuthConfig();
 
   /** Configuration object from the caCertificates bundle: @{@link CaCertificateConfiguration} */
   private CaCertificateConfiguration caCertificateConfiguration = new CaCertificateConfiguration();
@@ -27,5 +31,14 @@ public class MorphiaApplicationConfiguration extends Configuration {
 
   public void setMongo(MongoConfiguration mongo) {
     this.mongo = mongo;
+  }
+
+  public AuthConfig getAuth() {
+    return auth;
+  }
+
+  public MorphiaApplicationConfiguration setAuth(AuthConfig auth) {
+    this.auth = auth;
+    return this;
   }
 }
