5b93f5c73cc9a36805e94ae8e9f0243d791ab085
Christian Pleister <christian.pleister@sda.se>
2022-07-21 13:24:50 +0200

    fix(server-kafka): add unit test

---
+++ sda-commons-server-kafka/src/test/java/org/sdase/commons/server/kafka/producer/KafkaMessageProducerTest.java
@@ -0,0 +1,28 @@
+package org.sdase.commons.server.kafka.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.sdase.commons.server.kafka.prometheus.ProducerTopicMessageCounter;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class KafkaMessageProducerTest {
+
+  @Mock
+  KafkaProducer<String, String> mockProducer;
+
+  @Test
+  void shouldDelegateFlushCallToProducer() {
+    //given
+    KafkaMessageProducer<String, String> kafkaMessageProducer = new KafkaMessageProducer<>("topicName", mockProducer, new ProducerTopicMessageCounter(), "producerName");
+    //when
+    kafkaMessageProducer.flush();
+    //then
+    verify(mockProducer, times(1)).flush();
+  }
+}
