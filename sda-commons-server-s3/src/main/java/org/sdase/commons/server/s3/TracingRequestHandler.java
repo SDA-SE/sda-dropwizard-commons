c00b8b6444d94f50499b3d64f0011854bf26dd68
Marwen Saadaoui <saadaoui_mar@live.fr>
2022-08-26 12:25:46 +0200

    dependencies constraints

--- sda-commons-server-s3/src/main/java/org/sdase/commons/server/s3/TracingRequestHandler.java
+++ sda-commons-server-s3/src/main/java/org/sdase/commons/server/s3/TracingRequestHandler.java
@@ -3,10 +3,8 @@ package org.sdase.commons.server.s3;
 import com.amazonaws.AmazonWebServiceRequest;
 import com.amazonaws.Request;
 import com.amazonaws.Response;
-import com.amazonaws.handlers.HandlerContextKey;
 import com.amazonaws.handlers.RequestHandler2;
 import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.context.Scope;
 import io.opentelemetry.instrumentation.awssdk.v1_11.AwsSdkTelemetry;
 
 public class TracingRequestHandler extends RequestHandler2 {
@@ -19,9 +17,6 @@ public class TracingRequestHandler extends RequestHandler2 {
             .build()
             .newRequestHandler();
   }
-
-  public static final HandlerContextKey<Scope> SCOPE =
-      new HandlerContextKey<>(Scope.class.getName());
 
   @Override
   public void beforeRequest(Request<?> request) {
