7d3f261be8919745f6694408d391ee26e3eaa387
Marwen Saadaoui <saadaoui_mar@live.fr>
2022-09-12 00:31:50 +0200

    add tests

--- sda-commons-server-opentelemetry-example/src/test/java/org/sdase/server/opentelemetry/example/OpenTelemetryTracingAppIT.java
+++ sda-commons-server-opentelemetry-example/src/test/java/org/sdase/server/opentelemetry/example/OpenTelemetryTracingAppIT.java
@@ -1,6 +1,7 @@
 package org.sdase.server.opentelemetry.example;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
 import io.dropwizard.Configuration;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
@@ -8,7 +9,6 @@ import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.sdk.testing.junit5.OpenTelemetryExtension;
 import io.opentelemetry.sdk.trace.data.SpanData;
 import io.opentelemetry.sdk.trace.data.StatusData;
-import java.util.List;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import org.assertj.core.groups.Tuple;
@@ -30,63 +30,80 @@ class OpenTelemetryTracingAppIT {
   void shouldGetInstrumented() {
     String response = webTarget("/instrumented").request().get(String.class);
     assertThat(response).isEqualTo("Done!");
-    assertThat(OTEL.getSpans())
-        .isNotEmpty()
-        .hasSize(7)
-        .extracting(SpanData::getName)
-        // contains 5 nested tasks with span "someWork" and one parent "instrumentedWork" and one
-        // parent server "GET /instrumented"
-        .containsExactly(
-            "someWork",
-            "someWork",
-            "someWork",
-            "someWork",
-            "someWork",
-            "instrumentedWork",
-            "GET /instrumented");
+
+    await()
+        .untilAsserted(
+            () ->
+                assertThat(OTEL.getSpans())
+                    .isNotEmpty()
+                    .hasSize(7)
+                    .extracting(SpanData::getName)
+                    // contains 5 nested tasks with span "someWork" and one parent
+                    // "instrumentedWork" and one
+                    // parent server "GET /instrumented"
+                    .containsExactly(
+                        "someWork",
+                        "someWork",
+                        "someWork",
+                        "someWork",
+                        "someWork",
+                        "instrumentedWork",
+                        "GET /instrumented"));
+
+    ;
   }
 
   @Test
-  void shouldGetHelloWorld() {
+  void shouldGetRoot() {
     String response = webTarget("/").request().get(String.class);
     assertThat(response).isEqualTo("This api is not traced");
-    assertThat(OTEL.getSpans())
-        .isNotEmpty()
-        .extracting(SpanData::getName, s -> s.getInstrumentationScopeInfo().getName())
-        .contains(Tuple.tuple("GET /", "sda-commons.servlet"));
+
+    await()
+        .untilAsserted(
+            () ->
+                assertThat(OTEL.getSpans())
+                    .isNotEmpty()
+                    .extracting(SpanData::getName, s -> s.getInstrumentationScopeInfo().getName())
+                    .contains(Tuple.tuple("GET /", "sda-commons.servlet")));
   }
 
   @Test
   void shouldDoException() {
     Response response = webTarget("/exception").request().get();
     assertThat(response.getStatus()).isEqualTo(200);
-    List<SpanData> spans = OTEL.getSpans();
-    assertThat(spans)
-        .isNotEmpty()
-        .hasSize(2)
-        .extracting(SpanData::getStatus)
-        .extracting(StatusData::getStatusCode, StatusData::getDescription)
-        .contains(Tuple.tuple(StatusCode.ERROR, "Something bad happened!"));
+
+    await()
+        .untilAsserted(
+            () ->
+                assertThat(OTEL.getSpans())
+                    .isNotEmpty()
+                    .hasSize(2)
+                    .extracting(SpanData::getStatus)
+                    .extracting(StatusData::getStatusCode, StatusData::getDescription)
+                    .contains(Tuple.tuple(StatusCode.ERROR, "Something bad happened!")));
+    ;
   }
 
   @Test
   void shouldDoParam() {
     String response = webTarget("/param/foo").request().get(String.class);
     assertThat(response).isEqualTo("foo");
-    List<SpanData> spans = OTEL.getSpans();
-    assertThat(spans)
-        .isNotEmpty()
-        .hasSize(1)
-        .extracting(SpanData::getName)
-        .containsExactly("GET /param/{value}");
+    await()
+        .untilAsserted(
+            () ->
+                assertThat(OTEL.getSpans())
+                    .isNotEmpty()
+                    .hasSize(1)
+                    .extracting(SpanData::getName)
+                    .containsExactly("GET /param/{value}"));
   }
 
   @Test
   void shouldDoRecursive() {
     Response response = webTarget("/recursive").request().get();
     assertThat(response.getStatus()).isEqualTo(200);
-    List<SpanData> spans = OTEL.getSpans();
-    assertThat(spans).isNotEmpty().hasSizeGreaterThan(2);
+
+    await().untilAsserted(() -> assertThat(OTEL.getSpans()).isNotEmpty().hasSizeGreaterThan(2));
   }
 
   private WebTarget webTarget(String path) {
