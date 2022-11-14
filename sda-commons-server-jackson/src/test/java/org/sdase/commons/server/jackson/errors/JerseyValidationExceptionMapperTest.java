27d4643449840abcb366ee8dd1290a3588966f9a
christopher-cudennec <christopher.cudennec@sda-se.com>
2022-11-14 08:50:56 +0100

    fix(server-jackson): Use Jackson for creating error codes of `ApiInvalidParam`
    Removing old code that provided backwards compatibility with Guava.
    
    BREAKING CHANGE: This commit might change the error codes of the `ApiInvalidParam`
    in your `ApiError` that you receive on validation errors (422 response status).
    if validation annotations with subsequent uppercase letters are used in request payload resources, you might need to adapt the error code handling, e.g. from `I_S_B_N` to `ISBN`.
    Other examples: EAN, URL, CNPJ, CPF, NIP, PESEL, REGON, INN

--- sda-commons-server-jackson/src/test/java/org/sdase/commons/server/jackson/errors/JerseyValidationExceptionMapperTest.java
+++ sda-commons-server-jackson/src/test/java/org/sdase/commons/server/jackson/errors/JerseyValidationExceptionMapperTest.java
@@ -1,12 +1,13 @@
 package org.sdase.commons.server.jackson.errors;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assumptions.assumeThat;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ClassInfo;
+import io.github.classgraph.ScanResult;
 import java.util.stream.Stream;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.ISBN;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -17,26 +18,26 @@ class JerseyValidationExceptionMapperTest {
 
   @ParameterizedTest
   @MethodSource("testData")
-  void shouldConvertToSnakeCase(String given, String expected) {
+  void shouldConvertCustomDataToSnakeCase(String given, String expected) {
     String actual = JerseyValidationExceptionMapper.camelToUpperSnakeCase(given);
     assertThat(actual).isEqualTo(expected);
   }
 
   @ParameterizedTest
   @MethodSource("realTestData")
-  void differenceToDefault(String given) {
+  void shouldConvertValidationClassesToSnakeCase(String given) {
     String actual = JerseyValidationExceptionMapper.camelToUpperSnakeCase(given);
     String expected = new PropertyNamingStrategies.UpperSnakeCaseStrategy().translate(given);
-    // would fail on: EAN, ISBN, URL, CNPJ, CPF, NIP, PESEL, REGON, INN
-    assumeThat(actual).isEqualTo(expected);
+    assertThat(actual).isEqualTo(expected);
   }
 
+  /** Custom test data for edge case / custom testing. */
   static Stream<Arguments> testData() {
     return Stream.of(
-        Arguments.of("NotNull", "NOT_NULL"),
-        Arguments.of("Email", "EMAIL"),
-        Arguments.of("ISBN", "I_S_B_N"), // how Guava did it, default Jackson: "ISBN"
-        Arguments.of("ValidURI", "VALID_U_R_I"), // how Guava did it, default Jackson: "VALID_URI"
+        Arguments.of(NotNull.class.getSimpleName(), "NOT_NULL"),
+        Arguments.of(Email.class.getSimpleName(), "EMAIL"),
+        Arguments.of(ISBN.class.getSimpleName(), "ISBN"),
+        Arguments.of("ValidURI", "VALID_URI"),
         Arguments.of("GreaterThan10", "GREATER_THAN10"),
         Arguments.of("GreaterThan10AndLessThan231", "GREATER_THAN10_AND_LESS_THAN231"));
   }
@@ -44,16 +45,16 @@ class JerseyValidationExceptionMapperTest {
   static Stream<Arguments> realTestData() {
     String javaxConstraintsPackageName = NotNull.class.getPackage().getName();
     String hibernateConstraintsPackageName = ISBN.class.getPackage().getName();
-    return new ClassGraph()
-            .enableClassInfo()
-            .scan()
-            .getAllClasses()
-            .filter(
-                classInfo ->
-                    classInfo.getPackageName().startsWith(javaxConstraintsPackageName)
-                        || classInfo.getPackageName().startsWith(hibernateConstraintsPackageName))
-            .stream()
-            .map(ClassInfo::getSimpleName)
-            .map(Arguments::of);
+    try (ScanResult scanResult = new ClassGraph().enableClassInfo().scan()) {
+      return scanResult
+          .getAllClasses()
+          .filter(
+              classInfo ->
+                  classInfo.getPackageName().startsWith(javaxConstraintsPackageName)
+                      || classInfo.getPackageName().startsWith(hibernateConstraintsPackageName))
+          .stream()
+          .map(ClassInfo::getSimpleName)
+          .map(Arguments::of);
+    }
   }
 }
