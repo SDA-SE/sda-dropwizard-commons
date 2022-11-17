723f6cb55f024859488da5c3368213c4c1c61823
christopher-cudennec <christopher.cudennec@sda-se.com>
2022-11-17 07:52:19 +0100

    test(shared-error): upgrading tests to JUnit 5

--- sda-commons-shared-error/src/test/java/org/sdase/commons/shared/api/error/ApiInvalidParamTest.java
+++ sda-commons-shared-error/src/test/java/org/sdase/commons/shared/api/error/ApiInvalidParamTest.java
@@ -2,12 +2,12 @@ package org.sdase.commons.shared.api.error;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class ApiInvalidParamTest {
+class ApiInvalidParamTest {
 
   @Test
-  public void toStringShouldNotContainReason() {
+  void toStringShouldNotContainReason() {
     ApiInvalidParam value = new ApiInvalidParam("field", "reason", "errorCode");
     assertThat(value.toString()).contains("field", "errorCode").doesNotContain("reason");
   }
