c1c33d1ca5e660299fec275d7cdf0f8913fbd9c5
elbiocaetano <elbio.caetano@hotmail.com>
2022-11-18 10:04:04 +0100

    test(server-dropwizard): upgrading tests to JUnit 5

--- sda-commons-server-dropwizard/src/test/java/org/sdase/commons/server/dropwizard/lifecycle/ManagedShutdownListenerTest.java
+++ sda-commons-server-dropwizard/src/test/java/org/sdase/commons/server/dropwizard/lifecycle/ManagedShutdownListenerTest.java
@@ -1,16 +1,16 @@
 package org.sdase.commons.server.dropwizard.lifecycle;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.dropwizard.lifecycle.Managed;
 import java.util.concurrent.atomic.AtomicBoolean;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class ManagedShutdownListenerTest {
+class ManagedShutdownListenerTest {
 
   @Test
-  public void delegateToOnShutdownOnStop() throws Exception {
+  void delegateToOnShutdownOnStop() throws Exception {
     AtomicBoolean shutdownCalled = new AtomicBoolean();
     Managed managedObject = ManagedShutdownListener.onShutdown(() -> shutdownCalled.set(true));
     managedObject.stop();
@@ -18,7 +18,7 @@ public class ManagedShutdownListenerTest {
   }
 
   @Test
-  public void doNotDelegateToOnShutdownOnStart() throws Exception {
+  void doNotDelegateToOnShutdownOnStart() throws Exception {
     AtomicBoolean shutdownCalled = new AtomicBoolean();
     Managed managedObject = ManagedShutdownListener.onShutdown(() -> shutdownCalled.set(true));
     managedObject.start();
