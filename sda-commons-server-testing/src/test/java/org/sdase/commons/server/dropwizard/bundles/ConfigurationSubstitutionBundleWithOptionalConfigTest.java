408d8c2b18ecea9d47a57ff0bb3c8aa3a9fbf226
Christopher Cudennec <48206222+christopher-cudennec@users.noreply.github.com>
2022-11-18 16:26:00 +0100

    test(server-testing): upgrading tests to JUnit 5

--- sda-commons-server-testing/src/test/java/org/sdase/commons/server/dropwizard/bundles/ConfigurationSubstitutionBundleWithOptionalConfigTest.java
+++ sda-commons-server-testing/src/test/java/org/sdase/commons/server/dropwizard/bundles/ConfigurationSubstitutionBundleWithOptionalConfigTest.java
@@ -1,27 +1,30 @@
 package org.sdase.commons.server.dropwizard.bundles;
 
 import io.dropwizard.testing.ResourceHelpers;
-import io.dropwizard.testing.junit.DropwizardAppRule;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
 import org.assertj.core.api.Assertions;
-import org.junit.ClassRule;
-import org.junit.Test;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.sdase.commons.server.dropwizard.test.DropwizardApp;
 import org.sdase.commons.server.dropwizard.test.DropwizardConfig;
-import org.sdase.commons.server.testing.SystemPropertyRule;
+import org.sdase.commons.server.testing.SystemPropertyClassExtension;
 
-public class ConfigurationSubstitutionBundleWithOptionalConfigTest {
+class ConfigurationSubstitutionBundleWithOptionalConfigTest {
 
-  @ClassRule
-  public static final SystemPropertyRule SYSTEM_PROPERTY_RULE =
-      new SystemPropertyRule().setProperty("PROPERTY_ONE", "valueFromEnv");
+  @RegisterExtension
+  @Order(0)
+  static final SystemPropertyClassExtension SYSTEM_PROPERTY_RULE =
+      new SystemPropertyClassExtension().setProperty("PROPERTY_ONE", "valueFromEnv");
 
-  @ClassRule
-  public static final DropwizardAppRule<DropwizardConfig> DW =
-      new DropwizardAppRule<>(
+  @RegisterExtension
+  @Order(1)
+  static final DropwizardAppExtension<DropwizardConfig> DW =
+      new DropwizardAppExtension<>(
           DropwizardApp.class, ResourceHelpers.resourceFilePath("test-config.yaml"));
 
   @Test
-  public void shouldReplaceInNestedProperties() {
+  void shouldReplaceInNestedProperties() {
     Assertions.assertThat(DW.getConfiguration().getOptionalConfig().getProperty1())
         .isEqualTo("valueFromEnv");
   }
