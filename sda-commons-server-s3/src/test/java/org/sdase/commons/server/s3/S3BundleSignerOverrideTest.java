f16fcb1a341b683d9f0cbe4188f144f30cba6622
christopher-cudennec <christopher.cudennec@sda-se.com>
2022-11-18 13:30:15 +0100

    test(server-s3): upgrading tests to JUnit 5

--- sda-commons-server-s3/src/test/java/org/sdase/commons/server/s3/S3BundleSignerOverrideTest.java
+++ sda-commons-server-s3/src/test/java/org/sdase/commons/server/s3/S3BundleSignerOverrideTest.java
@@ -5,41 +5,43 @@ import static io.dropwizard.testing.ConfigOverride.randomPorts;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.amazonaws.services.s3.AmazonS3;
-import io.dropwizard.testing.junit.DropwizardAppRule;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
 import java.util.Date;
-import org.junit.ClassRule;
-import org.junit.Test;
-import org.junit.rules.RuleChain;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.sdase.commons.server.s3.test.Config;
 import org.sdase.commons.server.s3.test.TestApp;
-import org.sdase.commons.server.s3.testing.S3MockRule;
+import org.sdase.commons.server.s3.testing.S3ClassExtension;
 
-public class S3BundleSignerOverrideTest {
+class S3BundleSignerOverrideTest {
 
-  private static final S3MockRule S_3_MOCK_RULE =
-      S3MockRule.builder().putObject("bucket", "key", "data").build();
+  @RegisterExtension
+  @Order(0)
+  private static final S3ClassExtension S3 =
+      S3ClassExtension.builder().putObject("bucket", "key", "data").build();
 
-  private static final DropwizardAppRule<Config> DW =
-      new DropwizardAppRule<>(
+  @RegisterExtension
+  @Order(1)
+  private static final DropwizardAppExtension<Config> DW =
+      new DropwizardAppExtension<>(
           TestApp.class,
           null,
           randomPorts(),
-          config("s3Config.endpoint", S_3_MOCK_RULE::getEndpoint),
+          config("s3Config.endpoint", S3::getEndpoint),
           config("s3Config.accessKey", "access-key"),
           config("s3Config.secretKey", "secret-key"),
           config("s3Config.signerOverride", "S3SignerType"));
 
-  @ClassRule public static final RuleChain CHAIN = RuleChain.outerRule(S_3_MOCK_RULE).around(DW);
-
-  @Test()
-  public void shouldProvideClient() {
+  @Test
+  void shouldProvideClient() {
     AmazonS3 client = getClient();
 
     assertThat(client.getObject("bucket", "key").getObjectContent()).hasContent("data");
   }
 
-  @Test()
-  public void shouldGeneratePresignedUrl() {
+  @Test
+  void shouldGeneratePresignedUrl() {
     AmazonS3 client = getClient();
 
     assertThat(client.generatePresignedUrl("bucket", "key", new Date()).toString())
@@ -48,7 +50,6 @@ public class S3BundleSignerOverrideTest {
 
   private AmazonS3 getClient() {
     TestApp app = DW.getApplication();
-    S3Bundle bundle = app.getS3Bundle();
-    return bundle.getClient();
+    return app.getS3Bundle().getClient();
   }
 }
