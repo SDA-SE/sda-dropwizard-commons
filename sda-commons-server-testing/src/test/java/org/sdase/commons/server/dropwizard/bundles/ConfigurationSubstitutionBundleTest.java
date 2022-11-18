de12fbdac19d6d1301b24bc85e15865fb1d2874f
Christopher Cudennec <48206222+christopher-cudennec@users.noreply.github.com>
2022-11-18 16:26:00 +0100

    test(server-testing): upgrading tests to JUnit 5

--- sda-commons-server-testing/src/test/java/org/sdase/commons/server/dropwizard/bundles/ConfigurationSubstitutionBundleTest.java
+++ sda-commons-server-testing/src/test/java/org/sdase/commons/server/dropwizard/bundles/ConfigurationSubstitutionBundleTest.java
@@ -1,57 +1,58 @@
 package org.sdase.commons.server.dropwizard.bundles;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.dropwizard.testing.ResourceHelpers;
-import io.dropwizard.testing.junit.DropwizardAppRule;
-import org.assertj.core.api.Assertions;
-import org.junit.ClassRule;
-import org.junit.Test;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.sdase.commons.server.dropwizard.test.DropwizardApp;
 import org.sdase.commons.server.dropwizard.test.DropwizardConfig;
-import org.sdase.commons.server.testing.SystemPropertyRule;
+import org.sdase.commons.server.testing.SystemPropertyClassExtension;
 
-public class ConfigurationSubstitutionBundleTest {
+class ConfigurationSubstitutionBundleTest {
 
-  @ClassRule
-  public static final SystemPropertyRule ENV =
-      new SystemPropertyRule()
+  @RegisterExtension
+  @Order(0)
+  static final SystemPropertyClassExtension SYS =
+      new SystemPropertyClassExtension()
           .setProperty("ENV_REPLACEMENT", "valueFromEnv")
           .setProperty("EXAMPLE_SUFFIX", "hello");
 
-  @ClassRule
-  public static final DropwizardAppRule<DropwizardConfig> DW =
-      new DropwizardAppRule<>(
+  @RegisterExtension
+  @Order(1)
+  static final DropwizardAppExtension<DropwizardConfig> DW =
+      new DropwizardAppExtension<>(
           DropwizardApp.class, ResourceHelpers.resourceFilePath("test-config.yaml"));
 
   @Test
-  public void shouldReplaceWithoutDefaultDefined() {
-    Assertions.assertThat(DW.getConfiguration().getEnvReplaced()).isEqualTo("valueFromEnv");
+  void shouldReplaceWithoutDefaultDefined() {
+    assertThat(DW.getConfiguration().getEnvReplaced()).isEqualTo("valueFromEnv");
   }
 
   @Test
-  public void shouldReplaceWithDefaultDefined() {
-    Assertions.assertThat(DW.getConfiguration().getEnvWithDefaultReplaced())
-        .isEqualTo("valueFromEnv");
+  void shouldReplaceWithDefaultDefined() {
+    assertThat(DW.getConfiguration().getEnvWithDefaultReplaced()).isEqualTo("valueFromEnv");
   }
 
   @Test
-  public void shouldUseDefaultIfEnvNotDefined() {
-    Assertions.assertThat(DW.getConfiguration().getEnvDefault()).isEqualTo("DEFAULT");
+  void shouldUseDefaultIfEnvNotDefined() {
+    assertThat(DW.getConfiguration().getEnvDefault()).isEqualTo("DEFAULT");
   }
 
   @Test
-  public void shouldKeepPlaceholderIfNoEnvAndNoDefault() {
-    Assertions.assertThat(DW.getConfiguration().getEnvMissing()).isEqualTo("${ENV_MISSING}");
+  void shouldKeepPlaceholderIfNoEnvAndNoDefault() {
+    assertThat(DW.getConfiguration().getEnvMissing()).isEqualTo("${ENV_MISSING}");
   }
 
   @Test
-  public void conditionalConfig() {
-    Assertions.assertThat(DW.getConfiguration().getOptionalConfig()).isNotNull();
+  void conditionalConfig() {
+    assertThat(DW.getConfiguration().getOptionalConfig()).isNotNull();
   }
 
   @Test
-  public void shouldSupportNestedVariables() {
-    Assertions.assertThat(DW.getConfiguration().getExample())
-        .startsWith("bar-")
-        .isEqualTo("bar-hello");
+  void shouldSupportNestedVariables() {
+    assertThat(DW.getConfiguration().getExample()).startsWith("bar-").isEqualTo("bar-hello");
   }
 }
