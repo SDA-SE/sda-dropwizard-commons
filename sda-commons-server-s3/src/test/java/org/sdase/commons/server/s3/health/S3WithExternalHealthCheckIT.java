b9173aa4545eca71a9d5f2703365da3e9649414e
christopher-cudennec <christopher.cudennec@sda-se.com>
2022-11-18 13:30:15 +0100

    test(server-s3): upgrading tests to JUnit 5

--- sda-commons-server-s3/src/test/java/org/sdase/commons/server/s3/health/S3WithExternalHealthCheckIT.java
+++ sda-commons-server-s3/src/test/java/org/sdase/commons/server/s3/health/S3WithExternalHealthCheckIT.java
@@ -5,50 +5,52 @@ import static io.dropwizard.testing.ResourceHelpers.resourceFilePath;
 import static java.net.HttpURLConnection.HTTP_OK;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.dropwizard.testing.junit.DropwizardAppRule;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
 import java.util.SortedSet;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Test;
-import org.junit.rules.RuleChain;
-import org.junit.rules.TestRule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.sdase.commons.server.s3.S3Bundle;
 import org.sdase.commons.server.s3.test.Config;
 import org.sdase.commons.server.s3.test.S3WithExternalHealthCheckTestApp;
-import org.sdase.commons.server.s3.testing.S3MockRule;
+import org.sdase.commons.server.s3.testing.S3ClassExtension;
 
-public class S3WithExternalHealthCheckIT {
+class S3WithExternalHealthCheckIT {
 
-  private static final S3MockRule S3_MOCK = S3MockRule.builder().createBucket("testbucket").build();
+  @RegisterExtension
+  @Order(0)
+  private static final S3ClassExtension S3 =
+      S3ClassExtension.builder().createBucket("testbucket").build();
 
-  private static final DropwizardAppRule<Config> DW =
-      new DropwizardAppRule<>(
+  @RegisterExtension
+  @Order(1)
+  private static final DropwizardAppExtension<Config> DW =
+      new DropwizardAppExtension<>(
           S3WithExternalHealthCheckTestApp.class,
           resourceFilePath("test-config.yml"),
-          config("s3Config.endpoint", S3_MOCK::getEndpoint),
+          config("s3Config.endpoint", S3::getEndpoint),
           config("s3Bucket", "testbucket"));
-
-  @ClassRule public static final TestRule CHAIN = RuleChain.outerRule(S3_MOCK).around(DW);
 
   private S3WithExternalHealthCheckTestApp app;
   private WebTarget adminTarget;
 
-  @Before
-  public void init() {
+  @BeforeEach
+  void init() {
     app = DW.getApplication();
     adminTarget = DW.client().target(String.format("http://localhost:%d/", DW.getAdminPort()));
   }
 
   @Test
-  public void healthCheckShouldNotContainS3() {
+  void healthCheckShouldNotContainS3() {
     SortedSet<String> checks = app.healthCheckRegistry().getNames();
     assertThat(checks).isNotEmpty().doesNotContain(S3Bundle.S3_HEALTH_CHECK_NAME);
   }
 
   @Test
-  public void shouldReturnHealthCheckOk() {
+  void shouldReturnHealthCheckOk() {
     Response response = adminTarget.path("healthcheck").request().get();
     assertThat(response.getStatus()).isEqualTo(HTTP_OK);
     assertThat(response.readEntity(String.class))
