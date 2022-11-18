715da420deab8a89e17af88f800c202b0bf6bc69
elbiocaetano <elbio.caetano@hotmail.com>
2022-11-18 10:04:04 +0100

    test(server-dropwizard): upgrading tests to JUnit 5

--- sda-commons-server-dropwizard/src/test/java/org/sdase/commons/server/dropwizard/bundles/DefaultLoggingConfigurationBundleWithEmptyConfigTest.java
+++ sda-commons-server-dropwizard/src/test/java/org/sdase/commons/server/dropwizard/bundles/DefaultLoggingConfigurationBundleWithEmptyConfigTest.java
@@ -8,21 +8,22 @@ import io.dropwizard.Configuration;
 import io.dropwizard.logging.AppenderFactory;
 import io.dropwizard.logging.ConsoleAppenderFactory;
 import io.dropwizard.logging.DefaultLoggingFactory;
-import io.dropwizard.testing.junit.DropwizardAppRule;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.junit.ClassRule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.sdase.commons.server.dropwizard.bundles.test.LoggingTestApp;
 
-public class DefaultLoggingConfigurationBundleWithEmptyConfigTest {
+class DefaultLoggingConfigurationBundleWithEmptyConfigTest {
 
-  @ClassRule
-  public static final DropwizardAppRule<Configuration> DW =
-      new DropwizardAppRule<>(LoggingTestApp.class, resourceFilePath("with-empty-config.yaml"));
+  @RegisterExtension
+  private static final DropwizardAppExtension<Configuration> DW =
+      new DropwizardAppExtension<>(
+          LoggingTestApp.class, resourceFilePath("with-empty-config.yaml"));
 
-  @Test()
-  public void shouldNotApplyConsoleAppender() {
+  @Test
+  void shouldNotApplyConsoleAppender() {
     LoggingTestApp app = DW.getApplication();
     DefaultLoggingFactory defaultLoggingFactory =
         (DefaultLoggingFactory) app.getConfiguration().getLoggingFactory();
