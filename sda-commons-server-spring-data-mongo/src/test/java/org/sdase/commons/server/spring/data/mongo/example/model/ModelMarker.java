4033c9a1c4b99f422829b097fd689b9b8e0d3b77
Christopher Cudennec <48206222+christopher-cudennec@users.noreply.github.com>
2023-01-09 15:19:48 +0100

    feat(server-spring-data-mongo): New bundle for Spring Data Mongo
    BREAKING CHANGE: The Morphia module was replaced by Spring Data Mongo
    
    See the migration guide at: https://github.com/SDA-SE/sda-dropwizard-commons/blob/master/MIGRATION_GUIDE.md#4-sda-commons-server-morphia

---
+++ sda-commons-server-spring-data-mongo/src/test/java/org/sdase/commons/server/spring/data/mongo/example/model/ModelMarker.java
@@ -0,0 +1,4 @@
+package org.sdase.commons.server.spring.data.mongo.example.model;
+
+/** Just a interface used as marker to identify the package with the model classes. */
+public interface ModelMarker {}
