4acd94e6fcb467c8d7b0b89b91fc08ecb033a4c0
elbiocaetano <elbio.caetano@hotmail.com>
2023-01-23 12:56:09 +0100

    refactor: renaming spring data mongo configuration

--- sda-commons-server-spring-data-mongo/src/test/java/org/sdase/commons/server/spring/data/mongo/example/MyConfiguration.java
+++ sda-commons-server-spring-data-mongo/src/test/java/org/sdase/commons/server/spring/data/mongo/example/MyConfiguration.java
@@ -3,21 +3,20 @@ package org.sdase.commons.server.spring.data.mongo.example;
 import io.dropwizard.Configuration;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import org.sdase.commons.server.spring.data.mongo.SpringDataMongoConfiguration;
+import org.sdase.commons.server.spring.data.mongo.MongoConfiguration;
 import org.sdase.commons.shared.certificates.ca.CaCertificateConfiguration;
 
 public class MyConfiguration extends Configuration {
 
-  @Valid @NotNull
-  private SpringDataMongoConfiguration springDataMongo = new SpringDataMongoConfiguration();
+  @Valid @NotNull private MongoConfiguration springDataMongo = new MongoConfiguration();
 
   private CaCertificateConfiguration config = new CaCertificateConfiguration();
 
-  public SpringDataMongoConfiguration getSpringDataMongo() {
+  public MongoConfiguration getSpringDataMongo() {
     return springDataMongo;
   }
 
-  public MyConfiguration setSpringDataMongo(SpringDataMongoConfiguration springDataMongo) {
+  public MyConfiguration setSpringDataMongo(MongoConfiguration springDataMongo) {
     this.springDataMongo = springDataMongo;
     return this;
   }
