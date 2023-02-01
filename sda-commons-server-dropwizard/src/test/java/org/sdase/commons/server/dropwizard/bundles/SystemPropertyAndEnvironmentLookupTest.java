58477873559e74a0de1157406e6429154a2e7d7e
Jörg Siebahn <joerg.siebahn@sda-se.com>
2023-02-01 19:48:24 +0100

    feat(server-dropwizard): write valid json strings in `config.yml` with `| toJsonString` operator

--- sda-commons-server-dropwizard/src/test/java/org/sdase/commons/server/dropwizard/bundles/SystemPropertyAndEnvironmentLookupTest.java
+++ sda-commons-server-dropwizard/src/test/java/org/sdase/commons/server/dropwizard/bundles/SystemPropertyAndEnvironmentLookupTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledForJreRange;
 import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.junitpioneer.jupiter.SetEnvironmentVariable;
 import org.junitpioneer.jupiter.SetSystemProperty;
 
@@ -14,6 +16,43 @@ class SystemPropertyAndEnvironmentLookupTest {
   @SetSystemProperty(key = "FOO", value = "bar")
   void shouldLookupSystemProperties() {
     assertThat(new SystemPropertyAndEnvironmentLookup().lookup("FOO")).isEqualTo("bar");
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"FOO", "FOO | toJsonString"})
+  void shouldReturnNullForMissingSystemProperty(String given) {
+    assertThat(new SystemPropertyAndEnvironmentLookup().lookup(given)).isNull();
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"FOO", "FOO | toJsonString"})
+  void shouldProvideTheBareKeyToLookup(String given) {
+    assertThat(SystemPropertyAndEnvironmentLookup.extractKeyToLookup(given)).isEqualTo("FOO");
+  }
+
+  @ParameterizedTest
+  @SetSystemProperty(key = "FOO", value = "a\"string that\nneeds\\escaping")
+  @ValueSource(
+      strings = {
+        "FOO | toJsonString",
+        "FOO| toJsonString",
+        "FOO | toJsonString ",
+        "FOO |toJsonString",
+        "FOO|toJsonString",
+        "FOO | TOJSONSTRING",
+        "FOO| TOJSONSTRING",
+        "FOO | TOJSONSTRING ",
+        "FOO |TOJSONSTRING",
+        "FOO|TOJSONSTRING",
+        "FOO | tojsonstring",
+        "FOO| tojsonstring",
+        "FOO | tojsonstring ",
+        "FOO |tojsonstring",
+        "FOO|tojsonstring"
+      })
+  void shouldLookupSystemPropertiesWithEscaping(String propertyDeclaration) {
+    assertThat(new SystemPropertyAndEnvironmentLookup().lookup(propertyDeclaration))
+        .isEqualTo("\"a\\\"string that\\nneeds\\\\escaping\"");
   }
 
   @DisabledForJreRange(min = JRE.JAVA_16)
