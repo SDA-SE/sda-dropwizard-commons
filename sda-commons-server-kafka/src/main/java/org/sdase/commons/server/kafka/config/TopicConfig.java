5157e294fcad00efdd009c7c9a8cab2ece497b7c
Elbio Caetano <elbio.caetano@hotmail.com>
2023-02-07 17:15:01 +0100

    fix(server-kafka): removing deprecated topicana package and classes
    BREAKING CHANGE:
    removed the topicana classes in package `org.sdase.commons.server.kafka.topicana`;
    removed method `checkTopicConfiguration` and changed the methods `forTopic(ExpectedTopicConfiguration topic)` to `forTopic(TopicConfig topic)` and `forTopics(Collection<ExpectedTopicConfiguration> topics)` to `forTopics(Collection<TopicConfig> topics)` on `org.sdase.commons.server.kafka.builder.MessageListenerRegistration` and `org.sdase.commons.server.kafka.builder.ProducerRegistration`.

--- sda-commons-server-kafka/src/main/java/org/sdase/commons/server/kafka/config/TopicConfig.java
+++ sda-commons-server-kafka/src/main/java/org/sdase/commons/server/kafka/config/TopicConfig.java
@@ -1,33 +1,13 @@
 package org.sdase.commons.server.kafka.config;
 
-import java.util.HashMap;
-import java.util.Map;
 import javax.validation.constraints.NotNull;
 
 public class TopicConfig {
 
   @NotNull private String name;
 
-  @NotNull private Integer replicationFactor = 1;
-
-  @NotNull private Integer partitions = 1;
-
-  private Map<String, String> config = new HashMap<>();
-
   public String getName() {
     return name;
-  }
-
-  public Integer getReplicationFactor() {
-    return replicationFactor;
-  }
-
-  public Integer getPartitions() {
-    return partitions;
-  }
-
-  public Map<String, String> getConfig() {
-    return config;
   }
 
   public TopicConfig setName(String name) {
@@ -35,30 +15,11 @@ public class TopicConfig {
     return this;
   }
 
-  public void setReplicationFactor(Integer replicationFactor) {
-    this.replicationFactor = replicationFactor;
-  }
-
-  public void setPartitions(Integer partitions) {
-    this.partitions = partitions;
-  }
-
-  public void setConfig(Map<String, String> config) {
-    this.config = config;
-  }
-
   public interface TopicNameBuilder {
     TopicConfigBuilder name(String name);
   }
 
   public interface TopicConfigBuilder {
-
-    TopicConfigBuilder withReplicationFactor(Integer replicationFactor);
-
-    TopicConfigBuilder withPartitions(Integer partitions);
-
-    TopicConfigBuilder addConfig(String key, String value);
-
     TopicConfig build();
   }
 
@@ -69,9 +30,6 @@ public class TopicConfig {
   public static class Builder implements TopicConfigBuilder, TopicNameBuilder {
 
     private String name;
-    private Integer partitions = 1;
-    private Integer replicationFactor = 1;
-    private Map<String, String> config = new HashMap<>();
 
     @Override
     public TopicConfigBuilder name(String name) {
@@ -80,30 +38,9 @@ public class TopicConfig {
     }
 
     @Override
-    public TopicConfigBuilder withReplicationFactor(Integer replicationFactor) {
-      this.replicationFactor = replicationFactor;
-      return this;
-    }
-
-    @Override
-    public TopicConfigBuilder withPartitions(Integer partitions) {
-      this.partitions = partitions;
-      return this;
-    }
-
-    @Override
-    public TopicConfigBuilder addConfig(String key, String value) {
-      config.put(key, value);
-      return this;
-    }
-
-    @Override
     public TopicConfig build() {
       TopicConfig topicConfig = new TopicConfig();
       topicConfig.setName(name);
-      topicConfig.setConfig(config);
-      topicConfig.setPartitions(partitions);
-      topicConfig.setReplicationFactor(replicationFactor);
       return topicConfig;
     }
   }
