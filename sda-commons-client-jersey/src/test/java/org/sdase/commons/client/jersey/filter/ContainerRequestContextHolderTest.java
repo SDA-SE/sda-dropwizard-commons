da497bb74e318f5d146674d2c7c766ab88f9258c
Jörg Siebahn <joerg.siebahn@sda-se.com>
2023-02-10 11:09:22 +0100

    feat(client-jersey): transfer MetadataContext when request context is transferred

--- sda-commons-client-jersey/src/test/java/org/sdase/commons/client/jersey/filter/ContainerRequestContextHolderTest.java
+++ sda-commons-client-jersey/src/test/java/org/sdase/commons/client/jersey/filter/ContainerRequestContextHolderTest.java
@@ -5,12 +5,15 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.sdase.commons.client.jersey.filter.ContainerRequestContextHolder.transferRequestContext;
 
 import java.net.URI;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import org.glassfish.jersey.internal.MapPropertiesDelegate;
 import org.glassfish.jersey.server.ContainerRequest;
 import org.junit.jupiter.api.Test;
+import org.sdase.commons.server.dropwizard.metadata.DetachedMetadataContext;
+import org.sdase.commons.server.dropwizard.metadata.MetadataContext;
 import org.slf4j.MDC;
 
 class ContainerRequestContextHolderTest {
@@ -48,6 +51,46 @@ class ContainerRequestContextHolderTest {
                       assertThat(MDC.get("Trace-Token")).isEqualTo("a-trace-token");
                       assertThat(new AuthHeaderClientFilter().getHeaderValue())
                           .hasValue("an-access-token");
+                      return 42;
+                    }))
+            .get();
+
+    assertThat(result).isEqualTo(42);
+
+    executorService.shutdown();
+  }
+
+  @Test
+  void shouldTransferMetadataContextToThreadForRunnable()
+      throws InterruptedException, ExecutionException {
+    initializeMetadataContext();
+
+    ExecutorService executorService = Executors.newFixedThreadPool(1);
+    executorService
+        .submit(
+            transferRequestContext(
+                () -> {
+                  assertThat(MetadataContext.current().valuesByKey("tenant-id"))
+                      .isEqualTo(List.of("tenant-1"));
+                }))
+        .get();
+
+    executorService.shutdown();
+  }
+
+  @Test
+  void shouldTransferMetadataContextToThreadForCallable()
+      throws ExecutionException, InterruptedException {
+    initializeMetadataContext();
+
+    ExecutorService executorService = Executors.newFixedThreadPool(1);
+    int result =
+        executorService
+            .submit(
+                transferRequestContext(
+                    () -> {
+                      assertThat(MetadataContext.current().valuesByKey("tenant-id"))
+                          .isEqualTo(List.of("tenant-1"));
                       return 42;
                     }))
             .get();
@@ -107,5 +150,11 @@ class ContainerRequestContextHolderTest {
             new MapPropertiesDelegate());
     containerRequest.header(AUTHORIZATION, "an-access-token");
     new ContainerRequestContextHolder().filter(containerRequest);
+  }
+
+  private void initializeMetadataContext() {
+    var detachedMetadataContext = new DetachedMetadataContext();
+    detachedMetadataContext.put("tenant-id", List.of("tenant-1"));
+    MetadataContext.createContext(detachedMetadataContext);
   }
 }
