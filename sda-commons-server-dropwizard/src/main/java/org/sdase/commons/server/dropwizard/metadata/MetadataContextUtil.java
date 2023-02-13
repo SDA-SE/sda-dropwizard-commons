b7dbb5cc8561e7f0a6fe4cd11eb70abde267dd90
Jörg Siebahn <joerg.siebahn@sda-se.com>
2023-02-13 11:30:25 +0100

    feat(server-dropwizard): provide helpers to merge metadata contexts

--- sda-commons-server-dropwizard/src/main/java/org/sdase/commons/server/dropwizard/metadata/MetadataContextUtil.java
+++ sda-commons-server-dropwizard/src/main/java/org/sdase/commons/server/dropwizard/metadata/MetadataContextUtil.java
@@ -1,10 +1,12 @@
 package org.sdase.commons.server.dropwizard.metadata;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.apache.commons.lang3.StringUtils;
 
 class MetadataContextUtil {
@@ -53,6 +55,52 @@ class MetadataContextUtil {
     return determineConfiguredValue(MetadataContext.METADATA_FIELDS_ENVIRONMENT_VARIABLE)
         .map(MetadataContextUtil::toSet)
         .orElse(Set.of());
+  }
+
+  static DetachedMetadataContext merge(
+      DetachedMetadataContext context1, DetachedMetadataContext context2) {
+    Set<String> keys =
+        Stream.concat(context1.keySet().stream(), context2.keySet().stream())
+            .collect(Collectors.toSet());
+    var result = new DetachedMetadataContext();
+    for (String key : keys) {
+      var normalizedValues1 = normalizeValues(context1.get(key));
+      var normalizedValues2 = normalizeValues(context2.get(key));
+      var newValues =
+          Stream.concat(normalizedValues1, normalizedValues2)
+              .distinct()
+              .collect(Collectors.toList());
+      result.put(key, newValues);
+    }
+    return result;
+  }
+
+  static DetachedMetadataContext mergeWithPreference(
+      DetachedMetadataContext preferredContextData, DetachedMetadataContext secondaryContextData) {
+    Set<String> keys =
+        Stream.concat(
+                preferredContextData.keySet().stream(), secondaryContextData.keySet().stream())
+            .collect(Collectors.toSet());
+    var result = new DetachedMetadataContext();
+    for (String key : keys) {
+      var preferredValues =
+          normalizeValues(preferredContextData.get(key)).collect(Collectors.toList());
+      if (!preferredValues.isEmpty()) {
+        result.put(key, preferredValues);
+      } else {
+        var secondaryValues =
+            normalizeValues(secondaryContextData.get(key)).collect(Collectors.toList());
+        result.put(key, secondaryValues);
+      }
+    }
+    return result;
+  }
+
+  private static Stream<String> normalizeValues(List<String> values) {
+    if (values == null) {
+      return Stream.of();
+    }
+    return values.stream().filter(StringUtils::isNotBlank);
   }
 
   private static Set<String> toSet(String commaDelimitedValues) {
