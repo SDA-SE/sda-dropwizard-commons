9e8e2d44a9966bb3f0b208b35883739be73f5456
Jörg Siebahn <joerg.siebahn@sda-se.com>
2023-02-14 10:25:17 +0100

    chore(server-kafka): simplify MessageProducer interface

--- sda-commons-server-kafka/src/test/java/org/sdase/commons/server/kafka/producer/KafkaMessageProducerTest.java
+++ sda-commons-server-kafka/src/test/java/org/sdase/commons/server/kafka/producer/KafkaMessageProducerTest.java
@@ -2,10 +2,7 @@ package org.sdase.commons.server.kafka.producer;
 
 import static org.mockito.Mockito.*;
 
-import java.util.concurrent.Future;
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.RecordMetadata;
-import org.apache.kafka.common.header.Headers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -31,18 +28,7 @@ class KafkaMessageProducerTest {
 
   @Test
   void callingFlushShouldNotThrowExceptionAndMakeSonarHappy() {
-    MessageProducer messageProducer =
-        new MessageProducer() {
-          @Override
-          public Future<RecordMetadata> send(Object key, Object value) {
-            return null;
-          }
-
-          @Override
-          public Future<RecordMetadata> send(Object key, Object value, Headers headers) {
-            return null;
-          }
-        };
+    MessageProducer<Object, Object> messageProducer = (k, v, h) -> null;
     messageProducer.flush();
   }
 }
