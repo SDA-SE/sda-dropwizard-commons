fddf9554aaff6aac9303f200d7a622a17261ca53
Jörg Siebahn <joerg.siebahn@sda-se.com>
2023-02-16 19:16:49 +0100

    test(server-kafka): test MessageListenerStrategy regarding metadata context

---
+++ sda-commons-server-kafka/src/test/java/org/sdase/commons/server/kafka/consumer/strategies/MessageListenerStrategyMetadataContextIntegrationTest.java
@@ -0,0 +1,125 @@
+package org.sdase.commons.server.kafka.consumer.strategies;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.apache.kafka.clients.consumer.ConsumerRecord.NO_TIMESTAMP;
+import static org.apache.kafka.clients.consumer.ConsumerRecord.NULL_SIZE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.header.internals.RecordHeader;
+import org.apache.kafka.common.header.internals.RecordHeaders;
+import org.apache.kafka.common.record.TimestampType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.sdase.commons.server.dropwizard.metadata.DetachedMetadataContext;
+import org.sdase.commons.server.dropwizard.metadata.MetadataContext;
+import org.sdase.commons.server.kafka.consumer.ErrorHandler;
+import org.sdase.commons.server.kafka.consumer.MessageHandler;
+import org.sdase.commons.server.kafka.consumer.strategies.autocommit.AutocommitMLS;
+import org.sdase.commons.server.kafka.consumer.strategies.retryprocessingerror.RetryProcessingErrorMLS;
+import org.sdase.commons.server.kafka.consumer.strategies.synccommit.SyncCommitMLS;
+import org.sdase.commons.server.kafka.prometheus.ConsumerTopicMessageHistogram;
+
+class MessageListenerStrategyMetadataContextIntegrationTest {
+
+  public static final String TOPIC = "topic";
+
+  static List<DetachedMetadataContext> handledContexts = new ArrayList<>();
+
+  static MessageHandler<String, String> messageHandler =
+      (c) -> handledContexts.add(MetadataContext.detachedCurrent());
+
+  static MessageHandler<String, String> throwingMessageHandler =
+      (c) -> {
+        throw new RuntimeException();
+      };
+
+  static ErrorHandler<String, String> errorHandler =
+      (cr, e, c) -> handledContexts.add(MetadataContext.detachedCurrent());
+
+  @BeforeEach
+  void cleanResult() {
+    handledContexts.clear();
+  }
+
+  static Stream<Arguments> testInstances() {
+    return Stream.of(
+        // handled in message handler
+        Arguments.of(false, new AutocommitMLS<>(messageHandler, errorHandler)),
+        Arguments.of(false, new RetryProcessingErrorMLS<>(messageHandler, errorHandler)),
+        Arguments.of(false, new SyncCommitMLS<>(messageHandler, errorHandler)),
+        // handled in error handler
+        Arguments.of(false, new AutocommitMLS<>(throwingMessageHandler, errorHandler)),
+        Arguments.of(true, new RetryProcessingErrorMLS<>(throwingMessageHandler, errorHandler)),
+        Arguments.of(false, new SyncCommitMLS<>(throwingMessageHandler, errorHandler)));
+  }
+
+  @ParameterizedTest
+  @MethodSource("testInstances")
+  void shouldHandleContexts(
+      boolean stopsInTopic, MessageListenerStrategy<String, String> messageListenerStrategy) {
+    messageListenerStrategy.init(mock(ConsumerTopicMessageHistogram.class), Set.of("tenant-id"));
+    var recordsMap = new LinkedHashMap<TopicPartition, List<ConsumerRecord<String, String>>>();
+    recordsMap.put(
+        new TopicPartition(TOPIC, 1),
+        List.of(
+            consumerRecord("one", "one-value", List.of(Map.entry("tenant-id", "one"))),
+            consumerRecord("two", "two-value", List.of(Map.entry("tenant-id", "two")))));
+    recordsMap.put(
+        new TopicPartition(TOPIC, 2),
+        List.of(consumerRecord("three", "three-value", List.of(Map.entry("tenant-id", "three")))));
+    var givenRecords = new ConsumerRecords<>(recordsMap);
+
+    //noinspection unchecked
+    messageListenerStrategy.processRecords(givenRecords, mock(KafkaConsumer.class));
+
+    if (stopsInTopic) {
+      assertThat(handledContexts)
+          .hasSize(2)
+          .extracting(Map.class::cast)
+          .containsExactly(
+              Map.of("tenant-id", List.of("one")), Map.of("tenant-id", List.of("three")));
+    } else {
+      assertThat(handledContexts)
+          .hasSize(3)
+          .extracting(Map.class::cast)
+          .containsExactly(
+              Map.of("tenant-id", List.of("one")),
+              Map.of("tenant-id", List.of("two")),
+              Map.of("tenant-id", List.of("three")));
+    }
+  }
+
+  private ConsumerRecord<String, String> consumerRecord(
+      String key, String value, List<Map.Entry<String, String>> headers) {
+    return new ConsumerRecord<>(
+        TOPIC,
+        1,
+        1,
+        NO_TIMESTAMP,
+        TimestampType.NO_TIMESTAMP_TYPE,
+        NULL_SIZE,
+        NULL_SIZE,
+        key,
+        value,
+        new RecordHeaders(
+            headers.stream()
+                .map(e -> new RecordHeader(e.getKey(), e.getValue().getBytes(UTF_8)))
+                .collect(Collectors.toList())),
+        Optional.empty());
+  }
+}
