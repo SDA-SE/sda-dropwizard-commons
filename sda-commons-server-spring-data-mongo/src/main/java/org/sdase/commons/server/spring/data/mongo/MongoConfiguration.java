ce9ccc80b8fd6de93b7643d23346c44bd3dcab16
Preethi Venkatesh <75310484+preethivenkatesh-sdase@users.noreply.github.com>
2023-03-01 15:52:53 +0100

    fix: mongodb config options deprecated in favor of mongodb 'connectionString' (#2161)

--- sda-commons-server-spring-data-mongo/src/main/java/org/sdase/commons/server/spring/data/mongo/MongoConfiguration.java
+++ sda-commons-server-spring-data-mongo/src/main/java/org/sdase/commons/server/spring/data/mongo/MongoConfiguration.java
@@ -20,7 +20,10 @@ public class MongoConfiguration {
    * <p>Details in the <a
    * href="https://docs.mongodb.com/manual/reference/connection-string/">connection string
    * documentation</a> for host1 to hostN.
+   *
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
    */
+  @Deprecated(forRemoval = true)
   private String hosts;
 
   /**
@@ -29,7 +32,10 @@ public class MongoConfiguration {
    * <p>Details in the <a
    * href="https://docs.mongodb.com/manual/reference/connection-string/">connection string
    * documentation</a> for database.
+   *
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
    */
+  @Deprecated(forRemoval = true)
   private String database;
 
   /**
@@ -38,7 +44,10 @@ public class MongoConfiguration {
    * <p>Details in the <a
    * href="https://docs.mongodb.com/manual/reference/connection-string/">connection string
    * documentation</a> for options.
+   *
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
    */
+  @Deprecated(forRemoval = true)
   private String options = "";
 
   /**
@@ -47,7 +56,10 @@ public class MongoConfiguration {
    * <p>Details in the <a
    * href="https://docs.mongodb.com/manual/reference/connection-string/">connection string
    * documentation</a> for username:password.
+   *
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
    */
+  @Deprecated(forRemoval = true)
   private String username;
 
   /**
@@ -56,10 +68,18 @@ public class MongoConfiguration {
    * <p>Details in the <a
    * href="https://docs.mongodb.com/manual/reference/connection-string/">connection string
    * documentation</a> for username:password.
+   *
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
    */
+  @Deprecated(forRemoval = true)
   private String password;
 
-  /** If SSL should be used for the database connection. */
+  /**
+   * If SSL should be used for the database connection.
+   *
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
+   */
+  @Deprecated(forRemoval = true)
   private boolean useSsl;
 
   /**
@@ -69,6 +89,10 @@ public class MongoConfiguration {
    */
   private String connectionString;
 
+  /**
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
+   */
+  @Deprecated(forRemoval = true)
   public String getHosts() {
     if (StringUtils.isBlank(hosts) && StringUtils.isNotBlank(connectionString)) {
       return String.join(",", new ConnectionString(connectionString).getHosts());
@@ -76,11 +100,19 @@ public class MongoConfiguration {
     return hosts;
   }
 
+  /**
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
+   */
+  @Deprecated(forRemoval = true)
   public MongoConfiguration setHosts(String hosts) {
     this.hosts = hosts;
     return this;
   }
 
+  /**
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
+   */
+  @Deprecated(forRemoval = true)
   public String getDatabase() {
     if (StringUtils.isBlank(database) && StringUtils.isNotBlank(connectionString)) {
       return String.join(",", new ConnectionString(connectionString).getDatabase());
@@ -88,20 +120,36 @@ public class MongoConfiguration {
     return database;
   }
 
+  /**
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
+   */
+  @Deprecated(forRemoval = true)
   public MongoConfiguration setDatabase(String database) {
     this.database = database;
     return this;
   }
 
+  /**
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
+   */
+  @Deprecated(forRemoval = true)
   public String getOptions() {
     return options;
   }
 
+  /**
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
+   */
+  @Deprecated(forRemoval = true)
   public MongoConfiguration setOptions(String options) {
     this.options = options;
     return this;
   }
 
+  /**
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
+   */
+  @Deprecated(forRemoval = true)
   public String getUsername() {
     if (StringUtils.isBlank(username) && StringUtils.isNotBlank(connectionString)) {
       return String.join(",", new ConnectionString(connectionString).getUsername());
@@ -109,11 +157,19 @@ public class MongoConfiguration {
     return username;
   }
 
+  /**
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
+   */
+  @Deprecated(forRemoval = true)
   public MongoConfiguration setUsername(String username) {
     this.username = username;
     return this;
   }
 
+  /**
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
+   */
+  @Deprecated(forRemoval = true)
   public String getPassword() {
     if (StringUtils.isBlank(password) && StringUtils.isNotBlank(connectionString)) {
       char[] passwordChars = new ConnectionString(connectionString).getPassword();
@@ -124,15 +180,27 @@ public class MongoConfiguration {
     return password;
   }
 
+  /**
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
+   */
+  @Deprecated(forRemoval = true)
   public MongoConfiguration setPassword(String password) {
     this.password = password;
     return this;
   }
 
+  /**
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
+   */
+  @Deprecated(forRemoval = true)
   public boolean isUseSsl() {
     return useSsl;
   }
 
+  /**
+   * @deprecated All the Mongo config options will be removed in favor of 'connectionString'.
+   */
+  @Deprecated(forRemoval = true)
   public MongoConfiguration setUseSsl(boolean useSsl) {
     this.useSsl = useSsl;
     return this;
