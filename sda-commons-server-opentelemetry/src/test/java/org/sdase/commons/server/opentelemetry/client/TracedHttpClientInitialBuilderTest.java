58cbe4361fc5c3de47e27aa3c2ada2e757635bc5
Jörg Siebahn <joerg.siebahn@sda-se.com>
2023-04-14 19:42:17 +0200

    feat(opentelemetry): deprecate TracedHttpClientInitialBuilder to avoid breaking change

---
+++ sda-commons-server-opentelemetry/src/test/java/org/sdase/commons/server/opentelemetry/client/TracedHttpClientInitialBuilderTest.java
@@ -0,0 +1,25 @@
+package org.sdase.commons.server.opentelemetry.client;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.dropwizard.setup.Environment;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class TracedHttpClientInitialBuilderTest {
+  @Test
+  void shouldUseGivenOtel() {
+    var givenPropagators = Mockito.mock(ContextPropagators.class, Mockito.RETURNS_DEEP_STUBS);
+    var given = Mockito.mock(OpenTelemetry.class, Mockito.RETURNS_DEEP_STUBS);
+    Mockito.when(given.getPropagators()).thenReturn(givenPropagators);
+    @SuppressWarnings("removal")
+    var actual =
+        new TracedHttpClientInitialBuilder(
+                Mockito.mock(Environment.class, Mockito.RETURNS_DEEP_STUBS))
+            .usingTelemetryInstance(given)
+            .createBuilder();
+    assertThat(actual).extracting("propagators").isSameAs(givenPropagators);
+  }
+}
