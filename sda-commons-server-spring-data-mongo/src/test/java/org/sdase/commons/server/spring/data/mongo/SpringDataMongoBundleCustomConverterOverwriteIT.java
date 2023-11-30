2991716de97ed589fc8348abd36e8ebfd0a51a83
Jörg Siebahn <joerg.siebahn@sda-se.com>
2023-11-30 09:23:19 +0100

    test(mongo): fix mongodb versions in tests

--- sda-commons-server-spring-data-mongo/src/test/java/org/sdase/commons/server/spring/data/mongo/SpringDataMongoBundleCustomConverterOverwriteIT.java
+++ sda-commons-server-spring-data-mongo/src/test/java/org/sdase/commons/server/spring/data/mongo/SpringDataMongoBundleCustomConverterOverwriteIT.java
@@ -1,5 +1,7 @@
 package org.sdase.commons.server.spring.data.mongo;
 
+import static de.flapdoodle.embed.mongo.distribution.Version.Main.V4_4;
+import static de.flapdoodle.embed.mongo.distribution.Version.Main.V5_0;
 import static io.dropwizard.testing.ConfigOverride.config;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,7 +35,8 @@ abstract class SpringDataMongoBundleCustomConverterOverwriteIT {
   static class MongoDb44Test extends SpringDataMongoBundleCustomConverterOverwriteIT {
     @RegisterExtension
     @Order(0)
-    static final MongoDbClassExtension mongo = MongoDbClassExtension.builder().build();
+    static final MongoDbClassExtension mongo =
+        MongoDbClassExtension.builder().withVersion(V4_4).build();
 
     @RegisterExtension
     @Order(1)
@@ -57,7 +60,8 @@ abstract class SpringDataMongoBundleCustomConverterOverwriteIT {
   static class MongoDb50Test extends SpringDataMongoBundleCustomConverterOverwriteIT {
     @RegisterExtension
     @Order(0)
-    static final MongoDbClassExtension mongo = MongoDbClassExtension.builder().build();
+    static final MongoDbClassExtension mongo =
+        MongoDbClassExtension.builder().withVersion(V5_0).build();
 
     @RegisterExtension
     @Order(1)
