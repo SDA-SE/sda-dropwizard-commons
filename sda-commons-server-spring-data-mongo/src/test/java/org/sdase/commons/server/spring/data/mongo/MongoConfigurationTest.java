65fa29ac3c8bbfaf76db62913132c814d7bb512f
Eike Nils Knopp <eike.knopp@sda.se>
2023-12-05 11:54:27 +0100

    feat(server-spring-data-mongo): remove deprecated legacy configuration
    BREAKING CHANGE: The MongoDB connection can no longer be configured with individual properties like hosts and database. The database connection must be configured with connectionString. This change may result in a breaking change for services using this library.

--- sda-commons-server-spring-data-mongo/src/test/java/org/sdase/commons/server/spring/data/mongo/MongoConfigurationTest.java
+++ sda-commons-server-spring-data-mongo/src/test/java/org/sdase/commons/server/spring/data/mongo/MongoConfigurationTest.java
@@ -2,82 +2,16 @@ package org.sdase.commons.server.spring.data.mongo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import javax.validation.Validation;
 import org.junit.jupiter.api.Test;
 
 class MongoConfigurationTest {
 
   @Test
-  void shouldReturnDatabaseFromConnectionString() {
-    MongoConfiguration config =
-        new MongoConfiguration().setConnectionString("mongodb://sysop:moon@localhost/records");
-    assertThat(config.getDatabase()).isEqualTo("records");
-  }
-
-  @Test
-  void shouldReturnUsernameFromConnectionString() {
-    MongoConfiguration config =
-        new MongoConfiguration().setConnectionString("mongodb://sysop:moon@localhost/records");
-    assertThat(config.getUsername()).isEqualTo("sysop");
-  }
-
-  @Test
-  void shouldReturnPasswordFromConnectionString() {
-    MongoConfiguration config =
-        new MongoConfiguration().setConnectionString("mongodb://sysop:moon@localhost/records");
-    assertThat(config.getPassword()).isEqualTo("moon");
-  }
-
-  @Test
-  void shouldReturnNullPasswordFromConnectionString() {
-    MongoConfiguration config =
-        new MongoConfiguration()
-            .setConnectionString("mongodb://sysop@localhost/records?authMechanism=MONGODB-X509");
-    assertThat(config.getPassword()).isNull();
-  }
-
-  @Test
-  void shouldReturnHostsFromConnectionString() {
-    MongoConfiguration config =
-        new MongoConfiguration().setConnectionString("mongodb://sysop:moon@localhost/records");
-    assertThat(config.getHosts()).isEqualTo("localhost");
-  }
-
-  @Test
   void shouldBeValidIfConnectionStringWasSet() {
     MongoConfiguration config =
         new MongoConfiguration().setConnectionString("mongodb://sysop:moon@localhost/records");
-    assertThat(config.isValid()).isTrue();
-  }
 
-  @Test
-  void shouldBeValidIfConnectionStringWasSetPassword() {
-    MongoConfiguration config =
-        new MongoConfiguration().setConnectionString("mongodb://sysop:moon@localhost/records");
-    assertThat(config.getPassword()).isEqualTo("moon");
-  }
-
-  @Test
-  void shouldBeValidIfHostsAndDatabaseWasSet() {
-    MongoConfiguration config =
-        new MongoConfiguration().setHosts("localhost").setDatabase("records");
-    assertThat(config.isValid()).isTrue();
-  }
-
-  @Test
-  void shouldNotBeValidIfNoHostsAndDatabaseWereSet() {
-    MongoConfiguration config = new MongoConfiguration();
-    assertThat(config.isValid()).isFalse();
-  }
-
-  @Test
-  void shouldNotBeValidIfOnlyHostsWasSet() {
-    MongoConfiguration config = new MongoConfiguration().setHosts("localhost");
-    assertThat(config.isValid()).isFalse();
-  }
-
-  @Test
-  void shouldNotBeValidIfOnlyDatabaseWasSet() {
-    MongoConfiguration config = new MongoConfiguration().setDatabase("records");
-    assertThat(config.isValid()).isFalse();
+    assertThat(Validation.buildDefaultValidatorFactory().getValidator().validate(config)).isEmpty();
   }
 }
