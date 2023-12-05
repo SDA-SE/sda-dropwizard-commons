d6bac2bde2aef31f5c48b1e55e5edc6207860cd1
Eike Nils Knopp <eike.knopp@sda.se>
2023-12-05 17:35:39 +0100

    refactor: remove license header

--- sda-commons-shared-asyncapi/src/main/java/org/sdase/commons/shared/asyncapi/util/JsonNodeUtil.java
+++ sda-commons-shared-asyncapi/src/main/java/org/sdase/commons/shared/asyncapi/util/JsonNodeUtil.java
@@ -1,10 +1,3 @@
-/*
- * Copyright 2022- SDA SE Open Industry Solutions (https://www.sda.se)
- *
- * Use of this source code is governed by an MIT-style
- * license that can be found in the LICENSE file or at
- * https://opensource.org/licenses/MIT.
- */
 package org.sdase.commons.shared.asyncapi.util;
 
 import com.fasterxml.jackson.databind.JsonNode;
