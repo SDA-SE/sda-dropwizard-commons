2ef3df413a06fa174dde526777c6a9fdd23e26c4
Eike Nils Knopp <eike.knopp@sda.se>
2023-12-05 15:30:45 +0100

    fix(server-spring-data-mongo): switch import to jakarta

--- sda-commons-server-spring-data-mongo/src/test/java/org/sdase/commons/server/spring/data/mongo/MongoConfigurationTest.java
+++ sda-commons-server-spring-data-mongo/src/test/java/org/sdase/commons/server/spring/data/mongo/MongoConfigurationTest.java
@@ -2,7 +2,7 @@ package org.sdase.commons.server.spring.data.mongo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import javax.validation.Validation;
+import jakarta.validation.Validation;
 import org.junit.jupiter.api.Test;
 
 class MongoConfigurationTest {
