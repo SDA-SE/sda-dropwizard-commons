a2532c4c745ea6773fd5feb12cc1fc40afc8b99f
Steve Liesche <steve.liesche@sda.se>
2023-12-11 10:37:07 +0100

    chore: add comments as suggested in PR

--- sda-commons-client-jersey/src/test/java/org/sdase/commons/client/jersey/ApiClientTimeoutTest.java
+++ sda-commons-client-jersey/src/test/java/org/sdase/commons/client/jersey/ApiClientTimeoutTest.java
@@ -49,6 +49,7 @@ class ApiClientTimeoutTest {
 
   @BeforeAll
   static void beforeAll() {
+    //    Explicitly set the wiremock host and port, to keep configuration after reset in setUp()
     WireMock.configureFor("http", "localhost", WIRE.getPort());
   }
 
--- sda-commons-client-jersey/src/test/java/org/sdase/commons/client/jersey/test/ClientTraceTest.java
+++ sda-commons-client-jersey/src/test/java/org/sdase/commons/client/jersey/test/ClientTraceTest.java
@@ -47,6 +47,7 @@ class ClientTraceTest {
 
   @BeforeAll
   static void beforeAll() {
+    //    Explicitly set the wiremock host and port, to keep configuration after reset in setUp()
     WireMock.configureFor("http", "localhost", WIRE.getPort());
   }
 
