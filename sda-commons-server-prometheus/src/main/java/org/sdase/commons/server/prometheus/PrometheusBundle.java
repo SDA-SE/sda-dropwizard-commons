0ec8b1ce50f0a82721a16298134efae5df1a0a5f
Steve Liesche <steve.liesche@sda.se>
2023-12-12 11:47:51 +0100

    chore: rebase clean up

--- sda-commons-server-prometheus/src/main/java/org/sdase/commons/server/prometheus/PrometheusBundle.java
+++ sda-commons-server-prometheus/src/main/java/org/sdase/commons/server/prometheus/PrometheusBundle.java
@@ -33,6 +33,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import org.sdase.commons.server.prometheus.health.DropwizardHealthCheckMeters;
 import org.sdase.commons.server.prometheus.health.HealthCheckAsPrometheusMetricServlet;
 import org.sdase.commons.server.prometheus.metric.request.duration.RequestDurationFilter;
 import org.sdase.commons.server.prometheus.metric.request.duration.RequestDurationHistogramSpecification;
