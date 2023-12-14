637f307108598f009ca03dfc63443495fdc6059b
christopher-cudennec <christopher.cudennec@sda-se.com>
2023-12-14 10:57:33 +0100

    test(server-cloudevents): adapt to new AsyncAPI generator

--- sda-commons-server-cloudevents/src/test/java/org/sdase/commons/server/cloudevents/AsyncApiDocumentationTest.java
+++ sda-commons-server-cloudevents/src/test/java/org/sdase/commons/server/cloudevents/AsyncApiDocumentationTest.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
-import org.sdase.commons.server.cloudevents.app.consume.ContractCreatedEvent;
-import org.sdase.commons.server.cloudevents.app.produce.PartnerCreatedEvent;
 import org.sdase.commons.server.testing.GoldenFileAssertions;
 import org.sdase.commons.shared.asyncapi.AsyncApiGenerator;
 
@@ -15,9 +13,6 @@ class AsyncApiDocumentationTest {
     String expected =
         AsyncApiGenerator.builder()
             .withAsyncApiBase(getClass().getResource("/asyncapi-template.yml"))
-            .withSchema("/PartnerCreatedEvent", PartnerCreatedEvent.class)
-            .withSchema("/ContractCreatedEvent", ContractCreatedEvent.class)
-            .withSchema("/PlainCloudEvent", CloudEventV1.class)
             .generateYaml();
 
     // specify where you want your file to be stored
