b811d6eca6c157af1a6aaf17f95ddfddb42ee6ee
christopher-cudennec <christopher.cudennec@sda-se.com>
2023-12-20 16:56:51 +0100

    fix(server-mongo-testing): remove deprecated methods from MongoDb interface
    BREAKING CHANGE: remove deprecated methods from MongoDb interface
    
    Removed getters from the `MongoDb` interface which affects the `MongoDbClassExtension`.
    You can retrieve information about the database, username or password from the
    `ConnectionString` that is provided by `MongoDbClassExtension#getMongoConnectionString()`.

--- sda-commons-server-mongo-testing/src/test/java/org/sdase/commons/server/mongo/testing/MongoDbClassExtensionBuilderTest.java
+++ sda-commons-server-mongo-testing/src/test/java/org/sdase/commons/server/mongo/testing/MongoDbClassExtensionBuilderTest.java
@@ -1,21 +1,20 @@
 package org.sdase.commons.server.mongo.testing;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 class MongoDbClassExtensionBuilderTest {
 
+  @RegisterExtension
+  static final MongoDbClassExtension MONGO = MongoDbClassExtension.builder().build();
+
   @Test
   void shouldUseDefaultValues() {
-    assertThatCode(
-            () -> {
-              final MongoDbClassExtension extension = MongoDbClassExtension.builder().build();
-              assertThat(extension.getUsername()).isNotNull();
-              assertThat(extension.getPassword()).isNotNull();
-              assertThat(extension.getDatabase()).isNotNull();
-            })
-        .doesNotThrowAnyException();
+    assertThat(MONGO.getMongoConnectionString()).isNotNull();
+    assertThat(MONGO.getMongoConnectionString().getUsername()).isNotNull();
+    assertThat(MONGO.getMongoConnectionString().getPassword()).isNotNull();
+    assertThat(MONGO.getMongoConnectionString().getDatabase()).isNotNull();
   }
 }
