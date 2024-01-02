acf06395239c88dbb64f7c64f6b95b93d3887356
Jörg Siebahn <joerg.siebahn@sda-se.com>
2024-01-02 14:52:48 +0100

    test(weld-example): extend test case to verify application start and REST tests

--- sda-commons-server-weld-example/src/test/java/org/sdase/commons/server/weld/WeldExampleApplicationITest.java
+++ sda-commons-server-weld-example/src/test/java/org/sdase/commons/server/weld/WeldExampleApplicationITest.java
@@ -1,11 +1,13 @@
 package org.sdase.commons.server.weld;
 
 import static io.dropwizard.testing.ConfigOverride.randomPorts;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import io.dropwizard.Configuration;
-import org.assertj.core.api.Assertions;
+import javax.ws.rs.core.MediaType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.sdase.commons.server.weld.beans.UsageBean;
 import org.sdase.commons.server.weld.testing.WeldAppExtension;
 
 class WeldExampleApplicationITest {
@@ -17,6 +19,16 @@ class WeldExampleApplicationITest {
   @Test
   void shouldBeInjectedCorrectly() {
     WeldExampleApplication app = APP.getApplication();
-    Assertions.assertThat(app.getUsageBean()).isNotNull();
+    assertThat(app.getUsageBean()).isNotNull().isInstanceOf(UsageBean.class);
+  }
+
+  @Test
+  void shouldGetFromRest() {
+    String result =
+        APP.client()
+            .target(String.format("http://localhost:%d/someString", APP.getLocalPort()))
+            .request(MediaType.APPLICATION_JSON_TYPE)
+            .get(String.class);
+    assertThat(result).isEqualTo("some string");
   }
 }
