7a78c81825e1e958b27ac076a22ba248ea249e35
Jörg Siebahn <joerg.siebahn@sda-se.com>
2024-01-08 19:37:05 +0100

    test(weld-testing): fix weld tests

--- sda-commons-server-weld-example/src/main/java/org/sdase/commons/server/weld/beans/SimpleBean.java
+++ sda-commons-server-weld-example/src/main/java/org/sdase/commons/server/weld/beans/SimpleBean.java
@@ -1,14 +1,15 @@
 package org.sdase.commons.server.weld.beans;
 
-import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.inject.Named;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@ApplicationScoped
 public class SimpleBean {
 
-  @Inject private String someString;
+  @Inject
+  @Named("some-string")
+  private String someString;
 
   private static final Logger LOG = LoggerFactory.getLogger(SimpleBean.class);
 
