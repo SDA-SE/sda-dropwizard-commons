b8c039f0a425c3fd64f25216ddfa4202a1ae9577
Jörg Siebahn <joerg.siebahn@sda-se.com>
2024-01-08 19:37:05 +0100

    test(weld-testing): fix weld tests

--- sda-commons-server-weld-example/src/test/java/org/sdase/commons/server/weld/WeldJunit5AutoExtensionTest.java
+++ sda-commons-server-weld-example/src/test/java/org/sdase/commons/server/weld/WeldJunit5AutoExtensionTest.java
@@ -2,19 +2,21 @@ package org.sdase.commons.server.weld;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.jboss.weld.junit5.auto.AddBeanClasses;
+import org.jboss.weld.bootstrap.spi.BeanDiscoveryMode;
+import org.jboss.weld.junit5.auto.AddPackages;
 import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.jboss.weld.junit5.auto.SetBeanDiscoveryMode;
 import org.junit.jupiter.api.Test;
-import org.sdase.commons.server.weld.beans.ExampleProducer;
-import org.sdase.commons.server.weld.beans.UsageBean;
 
 @EnableAutoWeld
-@AddBeanClasses({ExampleProducer.class, UsageBean.class})
+@AddPackages(WeldExampleApplication.class)
+@SetBeanDiscoveryMode(BeanDiscoveryMode.ALL)
 class WeldJunit5AutoExtensionTest {
 
   @Test
   void shouldBeInjectedCorrectly(WeldExampleApplication application) {
     assertThat(application).isNotNull();
     assertThat(application.getUsageBean()).isNotNull();
+    assertThat(application.getSomeString()).isEqualTo("some string");
   }
 }
