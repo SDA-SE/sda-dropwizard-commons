f76628ddfdc400497cd3a715a1786cb86e18863b
Jörg Siebahn <joerg.siebahn@sda-se.com>
2024-01-08 10:49:20 +0100

    test(weld-example): migrate back to WeldAppExtension

--- sda-commons-server-weld-example/src/test/java/org/sdase/commons/server/weld/WeldExampleApplicationITest.java
+++ sda-commons-server-weld-example/src/test/java/org/sdase/commons/server/weld/WeldExampleApplicationITest.java
@@ -3,36 +3,19 @@ package org.sdase.commons.server.weld;
 import static io.dropwizard.testing.ConfigOverride.randomPorts;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.dropwizard.Application;
 import io.dropwizard.Configuration;
-import io.dropwizard.testing.DropwizardTestSupport;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
-import javax.enterprise.inject.spi.CDI;
 import javax.ws.rs.core.MediaType;
-import org.glassfish.jersey.ext.cdi1x.internal.CdiComponentProvider;
-import org.jboss.weld.junit5.auto.AddExtensions;
-import org.jboss.weld.junit5.auto.AddPackages;
-import org.jboss.weld.junit5.auto.EnableAutoWeld;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.sdase.commons.server.weld.beans.UsageBean;
+import org.sdase.commons.server.weld.testing.WeldAppExtension;
 
-@EnableAutoWeld
-@AddPackages(WeldExampleApplication.class)
-@AddExtensions(CdiComponentProvider.class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class WeldExampleApplicationITest {
 
   @RegisterExtension
   static DropwizardAppExtension<Configuration> APP =
-      new DropwizardAppExtension<>(
-          new DropwizardTestSupport<>(WeldExampleApplication.class, null, randomPorts()) {
-            @Override
-            public Application<Configuration> newApplication() {
-              return CDI.current().select(WeldExampleApplication.class).get();
-            }
-          });
+      new WeldAppExtension<>(WeldExampleApplication.class, null, randomPorts());
 
   @Test
   void shouldBeInjectedCorrectly() {
