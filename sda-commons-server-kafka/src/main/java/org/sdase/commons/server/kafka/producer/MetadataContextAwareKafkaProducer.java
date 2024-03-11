9635cc43cc627e01fb6db61c628f4d739d4f876f
Jörg Siebahn <joerg.siebahn@sda-se.com>
2024-03-11 17:52:21 +0100

    chore(kafka): extract duplicate code in abstract Producer implementation

--- sda-commons-server-kafka/src/main/java/org/sdase/commons/server/kafka/producer/MetadataContextAwareKafkaProducer.java
+++ sda-commons-server-kafka/src/main/java/org/sdase/commons/server/kafka/producer/MetadataContextAwareKafkaProducer.java
@@ -1,25 +1,13 @@
 package org.sdase.commons.server.kafka.producer;
 
 import java.nio.charset.StandardCharsets;
-import java.time.Duration;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.Future;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.kafka.clients.consumer.ConsumerGroupMetadata;
-import org.apache.kafka.clients.consumer.OffsetAndMetadata;
-import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
-import org.apache.kafka.common.Metric;
-import org.apache.kafka.common.MetricName;
-import org.apache.kafka.common.PartitionInfo;
-import org.apache.kafka.common.TopicPartition;
-import org.apache.kafka.common.Uuid;
-import org.apache.kafka.common.errors.ProducerFencedException;
+import org.apache.kafka.common.header.Headers;
 import org.apache.kafka.common.header.internals.RecordHeaders;
 import org.sdase.commons.server.dropwizard.metadata.MetadataContext;
 
@@ -32,9 +20,8 @@ import org.sdase.commons.server.dropwizard.metadata.MetadataContext;
  * @param <K> the type of the message key
  * @param <V> the type of the message value
  */
-public class MetadataContextAwareKafkaProducer<K, V> implements Producer<K, V> {
-
-  private final Producer<K, V> delegate;
+public class MetadataContextAwareKafkaProducer<K, V>
+    extends AbstractDelegatingAdditionalHeadersProducer<K, V> {
 
   private final Set<String> metadataFields;
 
@@ -43,90 +30,18 @@ public class MetadataContextAwareKafkaProducer<K, V> implements Producer<K, V> {
    * @param metadataFields the configured fields that are used as metadata
    */
   public MetadataContextAwareKafkaProducer(Producer<K, V> delegate, Set<String> metadataFields) {
-    this.delegate = delegate;
+    super(delegate);
     this.metadataFields = Optional.ofNullable(metadataFields).orElse(Set.of());
   }
 
   @Override
-  public void initTransactions() {
-    delegate.initTransactions();
-  }
-
-  @Override
-  public void beginTransaction() throws ProducerFencedException {
-    delegate.beginTransaction();
-  }
-
-  @Override
-  @SuppressWarnings("deprecation")
-  public void sendOffsetsToTransaction(
-      Map<TopicPartition, OffsetAndMetadata> offsets, String consumerGroupId)
-      throws ProducerFencedException {
-    delegate.sendOffsetsToTransaction(offsets, consumerGroupId);
-  }
-
-  @Override
-  public void sendOffsetsToTransaction(
-      Map<TopicPartition, OffsetAndMetadata> offsets, ConsumerGroupMetadata groupMetadata)
-      throws ProducerFencedException {
-    delegate.sendOffsetsToTransaction(offsets, groupMetadata);
-  }
-
-  @Override
-  public void commitTransaction() throws ProducerFencedException {
-    delegate.commitTransaction();
-  }
-
-  @Override
-  public void abortTransaction() throws ProducerFencedException {
-    delegate.abortTransaction();
-  }
-
-  @Override
-  public Future<RecordMetadata> send(ProducerRecord<K, V> producerRecord) {
-    return delegate.send(withMetadataContext(producerRecord));
-  }
-
-  @Override
-  public Future<RecordMetadata> send(ProducerRecord<K, V> producerRecord, Callback callback) {
-    return delegate.send(withMetadataContext(producerRecord), callback);
-  }
-
-  @Override
-  public void flush() {
-    delegate.flush();
-  }
-
-  @Override
-  public List<PartitionInfo> partitionsFor(String topic) {
-    return delegate.partitionsFor(topic);
-  }
-
-  @Override
-  public Map<MetricName, ? extends Metric> metrics() {
-    return delegate.metrics();
-  }
-
-  @Override
-  public Uuid clientInstanceId(Duration timeout) {
-    return delegate.clientInstanceId(timeout);
-  }
-
-  @Override
-  public void close() {
-    delegate.close();
-  }
-
-  @Override
-  public void close(Duration timeout) {
-    delegate.close(timeout);
-  }
-
-  private ProducerRecord<K, V> withMetadataContext(ProducerRecord<K, V> producerRecord) {
+  protected Headers additionalHeaders(ProducerRecord<K, V> producerRecord) {
     if (metadataFields.isEmpty()) {
-      return producerRecord;
+      return null;
     }
-    var headers = new RecordHeaders(producerRecord.headers());
+
+    Headers headers = new RecordHeaders();
+
     MetadataContext metadataContext = MetadataContext.current();
     for (String metadataField : metadataFields) {
       List<String> valuesByKey = metadataContext.valuesByKey(metadataField);
@@ -140,12 +55,6 @@ public class MetadataContextAwareKafkaProducer<K, V> implements Producer<K, V> {
           .map(v -> v.getBytes(StandardCharsets.UTF_8))
           .forEach(v -> headers.add(metadataField, v));
     }
-    return new ProducerRecord<>(
-        producerRecord.topic(),
-        producerRecord.partition(),
-        producerRecord.timestamp(),
-        producerRecord.key(),
-        producerRecord.value(),
-        headers);
+    return headers;
   }
 }
