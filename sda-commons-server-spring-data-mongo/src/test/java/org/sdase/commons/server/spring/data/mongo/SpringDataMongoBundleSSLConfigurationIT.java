fa13d5d2e26e8d25ce0966c1ae3b29b0e46aa4aa
Simon Friz <simon.friz@sda.se>
2025-05-09 13:00:39 +0200

    feat(mongodb-testing): increase default MongoDB version for unit tests from 4.4 to 6

--- sda-commons-server-spring-data-mongo/src/test/java/org/sdase/commons/server/spring/data/mongo/SpringDataMongoBundleSSLConfigurationIT.java
+++ sda-commons-server-spring-data-mongo/src/test/java/org/sdase/commons/server/spring/data/mongo/SpringDataMongoBundleSSLConfigurationIT.java
@@ -99,6 +99,84 @@ abstract class SpringDataMongoBundleSSLConfigurationIT {
     }
   }
 
+  static class MongoDb60Test extends SpringDataMongoBundleSSLConfigurationIT {
+    @RegisterExtension
+    @Order(0)
+    static final MongoDbClassExtension mongo =
+        MongoDbClassExtension.builder().withVersion(Version.Main.V6_0).build();
+
+    @RegisterExtension
+    @Order(1)
+    static final DropwizardAppExtension<MyConfiguration> DW =
+        new DropwizardAppExtension<>(
+            AutoIndexDisabledApp.class,
+            null,
+            randomPorts(),
+            config("springDataMongo.connectionString", mongo::getConnectionString),
+            config(
+                "config.customCaCertificateDir", Paths.get("src", "test", "resources").toString()));
+
+    @RegisterExtension
+    @Order(2)
+    static final DropwizardAppExtension<MyConfiguration> DW_WITHOUT_SSL =
+        new DropwizardAppExtension<>(
+            AutoIndexDisabledApp.class,
+            null,
+            randomPorts(),
+            config("springDataMongo.connectionString", mongo::getConnectionString),
+            // make sure certificates dir is unset
+            config("config.customCaCertificateDir", ""));
+
+    @Override
+    DropwizardAppExtension<MyConfiguration> getDW() {
+      return DW;
+    }
+
+    @Override
+    DropwizardAppExtension<MyConfiguration> getDwWithoutSSL() {
+      return DW_WITHOUT_SSL;
+    }
+  }
+
+  static class MongoDb70Test extends SpringDataMongoBundleSSLConfigurationIT {
+    @RegisterExtension
+    @Order(0)
+    static final MongoDbClassExtension mongo =
+        MongoDbClassExtension.builder().withVersion(Version.Main.V7_0).build();
+
+    @RegisterExtension
+    @Order(1)
+    static final DropwizardAppExtension<MyConfiguration> DW =
+        new DropwizardAppExtension<>(
+            AutoIndexDisabledApp.class,
+            null,
+            randomPorts(),
+            config("springDataMongo.connectionString", mongo::getConnectionString),
+            config(
+                "config.customCaCertificateDir", Paths.get("src", "test", "resources").toString()));
+
+    @RegisterExtension
+    @Order(2)
+    static final DropwizardAppExtension<MyConfiguration> DW_WITHOUT_SSL =
+        new DropwizardAppExtension<>(
+            AutoIndexDisabledApp.class,
+            null,
+            randomPorts(),
+            config("springDataMongo.connectionString", mongo::getConnectionString),
+            // make sure certificates dir is unset
+            config("config.customCaCertificateDir", ""));
+
+    @Override
+    DropwizardAppExtension<MyConfiguration> getDW() {
+      return DW;
+    }
+
+    @Override
+    DropwizardAppExtension<MyConfiguration> getDwWithoutSSL() {
+      return DW_WITHOUT_SSL;
+    }
+  }
+
   @Test
   void shouldHaveCreatedSSLContext() {
     var sslContext = getSSLContext(getDW());
