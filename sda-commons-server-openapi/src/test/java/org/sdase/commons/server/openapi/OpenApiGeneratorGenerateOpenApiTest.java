dbc15561de37fe4a9d8e495e283b49bee47529b4
Piotr Kupiec <piotr.kupiec@sda.se>
2025-05-27 08:53:57 +0200

    ci: add new method to ignore api version when testing

--- sda-commons-server-openapi/src/test/java/org/sdase/commons/server/openapi/OpenApiGeneratorGenerateOpenApiTest.java
+++ sda-commons-server-openapi/src/test/java/org/sdase/commons/server/openapi/OpenApiGeneratorGenerateOpenApiTest.java
@@ -2,7 +2,9 @@ package org.sdase.commons.server.openapi;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.sdase.commons.server.openapi.OpenApiFileHelper.normalizeOpenApiYaml;
+import static org.sdase.commons.server.openapi.OpenApiFileHelper.normalizeOpenApiYamlAndReplaceVersion;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
@@ -21,8 +23,12 @@ class OpenApiGeneratorGenerateOpenApiTest {
     assertThat(openapi).isNotNull();
 
     Path expectedPath = Paths.get("src/test/resources/expected-openapi.yaml");
+    String actual = new String(Files.readAllBytes(expectedPath));
+
     GoldenFileAssertions.assertThat(expectedPath)
         .hasYamlContentAndUpdateGolden(normalizeOpenApiYaml(openapi));
+    GoldenFileAssertions.assertThat(expectedPath)
+        .hasYamlContentAndUpdateGolden(normalizeOpenApiYamlAndReplaceVersion(openapi, actual));
   }
 
   @Test
--- sda-commons-server-testing/src/main/java/org/sdase/commons/server/testing/GoldenFileAssertions.java
+++ sda-commons-server-testing/src/main/java/org/sdase/commons/server/testing/GoldenFileAssertions.java
@@ -10,7 +10,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import org.assertj.core.api.AbstractAssert;
 import org.assertj.core.api.Assertions;
-import org.sdase.commons.server.openapi.OpenApiFileHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -158,13 +157,12 @@ public class GoldenFileAssertions extends AbstractAssert<GoldenFileAssertions, P
     try {
       // assert if exists
       Assertions.assertThat(actual).as(ASSERTION_TEXT, fileName, fileName, fileName).exists();
-      String actualAsString = new String(Files.readAllBytes(actual));
 
       // assert YAML / JSON
       ObjectMapper objectMapper = Jackson.newObjectMapper(new YAMLFactory());
-      Assertions.assertThat(objectMapper.readTree(OpenApiFileHelper.removeInfoVersionFromOpenApiYaml(actualAsString)))
+      Assertions.assertThat(objectMapper.readTree(actual.toFile()))
           .as(ASSERTION_TEXT, fileName, fileName, fileName)
-          .isEqualTo(objectMapper.readTree(OpenApiFileHelper.removeInfoVersionFromOpenApiYaml(expected)));
+          .isEqualTo(objectMapper.readTree(expected));
     } finally {
       // eventually update the file content
       if (ciUtil.isRunningInCiPipeline()) {
