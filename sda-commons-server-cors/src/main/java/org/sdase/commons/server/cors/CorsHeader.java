c2560162956b8cf286618997194b754997e13534
Sage Maasch <sage.maasch@sda.se>
2025-09-22 18:02:33 +0200

    feat: replace deprecated CrossOriginFilter with CrossOriginHandler
    BREAKING CHANGE:
    Preflight behavior changes: ACCESS_CONTROL_ALLOW_METHODS_HEADER has to be checked by browser if method is allowed

---
+++ sda-commons-server-cors/src/main/java/org/sdase/commons/server/cors/CorsHeader.java
@@ -0,0 +1,22 @@
+package org.sdase.commons.server.cors;
+
+public class CorsHeader {
+
+  // Request headers
+  public static final String ORIGIN_HEADER = "Origin";
+  public static final String ACCESS_CONTROL_REQUEST_METHOD_HEADER = "Access-Control-Request-Method";
+  public static final String ACCESS_CONTROL_REQUEST_HEADERS_HEADER =
+      "Access-Control-Request-Headers";
+  // Response headers
+  public static final String ACCESS_CONTROL_ALLOW_ORIGIN_HEADER = "Access-Control-Allow-Origin";
+  public static final String ACCESS_CONTROL_ALLOW_METHODS_HEADER = "Access-Control-Allow-Methods";
+  public static final String ACCESS_CONTROL_ALLOW_HEADERS_HEADER = "Access-Control-Allow-Headers";
+  public static final String ACCESS_CONTROL_MAX_AGE_HEADER = "Access-Control-Max-Age";
+  public static final String ACCESS_CONTROL_ALLOW_CREDENTIALS_HEADER =
+      "Access-Control-Allow-Credentials";
+  public static final String ACCESS_CONTROL_EXPOSE_HEADERS_HEADER = "Access-Control-Expose-Headers";
+  public static final String TIMING_ALLOW_ORIGIN_HEADER = "Timing-Allow-Origin";
+
+  private CorsHeader() {
+  }
+}
