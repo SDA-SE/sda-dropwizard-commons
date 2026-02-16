21c1b620f5485d1d1a29723480f32ceec8d59992
Sage Maasch <sage.maasch@sda.se>
2026-02-16 12:00:55 +0100

    fix(sda-commons-shared-asyncapi): change naming strategy so that the generic type isn't in the name of the class in schema

--- sda-commons-shared-asyncapi/src/test/java/org/sdase/commons/shared/asyncapi/jsonschema/victools/VictoolsJsonSchemaBuilderTest.java
+++ sda-commons-shared-asyncapi/src/test/java/org/sdase/commons/shared/asyncapi/jsonschema/victools/VictoolsJsonSchemaBuilderTest.java
@@ -1,7 +1,13 @@
 package org.sdase.commons.shared.asyncapi.jsonschema.victools;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.Map;
 import java.util.Set;
+import org.junit.jupiter.api.Test;
 import org.sdase.commons.shared.asyncapi.jsonschema.AbstractJsonSchemaBuilderTest;
+import org.sdase.commons.shared.asyncapi.test.data.models.Plane;
 
 class VictoolsJsonSchemaBuilderTest extends AbstractJsonSchemaBuilderTest {
 
@@ -12,5 +18,11 @@ class VictoolsJsonSchemaBuilderTest extends AbstractJsonSchemaBuilderTest {
   @Override
   protected Set<DisabledSpec> disableSpecificFieldTests() {
     return Set.of();
+  }
+
+  @Test
+  void shouldNotPrintGenericTypeInName() {
+    Map<String, JsonNode> jsonSchema = jsonSchemaBuilder.toJsonSchema(Plane.class);
+    assertThat(jsonSchema).containsKey("Plane").doesNotContainKey("Plane(Object)");
   }
 }
